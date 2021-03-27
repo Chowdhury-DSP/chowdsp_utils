88b3ce23b36373e1a93aa1bfb1b0cabe20761353
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-03-26 20:58:31 -0400

    Fix Mac compilation warnings for DelayLine

--- DSP/Delay/chowdsp_DelayInterpolation.h
+++ DSP/Delay/chowdsp_DelayInterpolation.h
@@ -260,10 +260,15 @@ namespace DelayLineInterpolationTypes
             {
 #ifdef __SSE2__
                 if constexpr (std::is_same<T, float>::value)
-                    buff_reg = juce::dsp::SIMDRegister<T> (_mm_loadu_ps (&buffer[delayInt + i]));
+                    buff_reg = juce::dsp::SIMDRegister<T> (_mm_loadu_ps (&buffer[(size_t) delayInt + i]));
                 else
+<<<<<<< Updated upstream
                     buff_reg = juce::dsp::SIMDRegister<T> (_mm_loadu_pd (&buffer[delayInt + i]));
 #else
+=======
+                    buff_reg = juce::dsp::SIMDRegister<T> (_mm_loadu_pd (&buffer[(size_t) delayInt + i]));
+              #else
+>>>>>>> Stashed changes
                 auto* regPtr = reinterpret_cast<T*> (&buff_reg.value);
                 std::copy (&buffer[delayInt + i], &buffer[delayInt + i + juce::dsp::SIMDRegister<T>::size()], regPtr);
 #endif
