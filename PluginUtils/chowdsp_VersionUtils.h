a719a58bc4dddb449bc9d4ac39f348bab7401319
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-09-22 02:22:23 -0700

    Add new class for handling version parsing

--- PluginUtils/chowdsp_VersionUtils.h
+++ PluginUtils/chowdsp_VersionUtils.h
@@ -4,48 +4,28 @@ namespace chowdsp
 {
 namespace VersionUtils
 {
-    /**
-     *  Method to compare two versions.
-     *  Returns 1 if v2 is smaller, -1 if v1 is smaller, 0 if equal
-     *  Adapted from: https://www.geeksforgeeks.org/compare-two-version-numbers/
-     */
-    inline int compareVersions (juce::String v1, juce::String v2)
+    /** Utility class to manage version strings. */
+    class Version
     {
-        v1.removeCharacters ("v");
-        v2.removeCharacters ("v");
+    public:
+        Version (const juce::String& versionStr);
 
-        // vnum stores each numeric part of version
-        int vnum1 = 0, vnum2 = 0;
+        juce::String getVersionString() const;
 
-        // loop untill both string are processed
-        for (int i = 0, j = 0; (i < v1.length() || j < v2.length());)
-        {
-            // storing numeric part of version 1 in vnum1
-            while (i < v1.length() && v1[i] != '.')
-            {
-                vnum1 = vnum1 * 10 + (v1[i] - '0');
-                i++;
-            }
+        friend bool operator== (const Version& v1, const Version& v2);
+        friend bool operator!= (const Version& v1, const Version& v2);
+        friend bool operator> (const Version& v1, const Version& v2);
+        friend bool operator< (const Version& v1, const Version& v2);
+        friend bool operator>= (const Version& v1, const Version& v2);
+        friend bool operator<= (const Version& v1, const Version& v2);
 
-            // storing numeric part of version 2 in vnum2
-            while (j < v2.length() && v2[j] != '.')
-            {
-                vnum2 = vnum2 * 10 + (v2[j] - '0');
-                j++;
-            }
+    private:
+        int major = 0;
+        int minor = 0;
+        int patch = 0;
 
-            if (vnum1 > vnum2)
-                return 1;
-            if (vnum2 > vnum1)
-                return -1;
-
-            // if equal, reset variables and go for next numeric part
-            vnum1 = vnum2 = 0;
-            i++;
-            j++;
-        }
-        return 0;
-    }
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Version)
+    };
 
 } // namespace VersionUtils
 
