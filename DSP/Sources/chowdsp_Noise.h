98a887c8edf1221edfecd3aac1f52897be8651e3
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2021-10-12 01:56:24 -0600

    Fix SVF to work with SIMD types (#57)
    * FixSVF to work with SIMD types
    
    * Apply clang-format
    
    Co-authored-by: jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- DSP/Sources/chowdsp_Noise.h
+++ DSP/Sources/chowdsp_Noise.h
@@ -86,26 +86,6 @@ private:
         }
     }
 
-    template <typename C = T>
-    inline typename std::enable_if<std::is_floating_point<C>::value, void>::type
-        copyBlocks (juce::dsp::AudioBlock<T>& dest, const juce::dsp::AudioBlock<const T>& src)
-    {
-        dest.copyFrom (src);
-    }
-
-    template <typename C = T>
-    inline typename std::enable_if<! std::is_floating_point<C>::value, void>::type
-        copyBlocks (juce::dsp::AudioBlock<T>& dest, const juce::dsp::AudioBlock<const T>& src)
-    {
-        for (size_t ch = 0; ch < dest.getNumChannels(); ++ch)
-        {
-            auto* srcPtr = src.getChannelPointer (ch);
-            auto* destPtr = dest.getChannelPointer (ch);
-            for (size_t i = 0; i < dest.getNumSamples(); ++i)
-                destPtr[i] = srcPtr[i];
-        }
-    }
-
     NoiseType type;
     juce::Random rand;
     juce::dsp::Gain<T> gain;
