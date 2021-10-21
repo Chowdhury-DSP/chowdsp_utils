7a9e0b4a91516ce3855381a07f24ead2aa7c409e
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-10-20 18:57:13 -0600

    Fix missing juce::

--- DSP/Delay/chowdsp_BBDDelayLine.h
+++ DSP/Delay/chowdsp_BBDDelayLine.h
@@ -60,7 +60,7 @@ namespace BBD
         void setDelayTime (float delaySec) noexcept
         {
             // if Ts_bbd == 0, then we get an infinite loop, so limit the min. delay to 1 sample
-            delaySec = jmax (Ts, delaySec);
+            delaySec = juce::jmax (Ts, delaySec);
 
             const auto clock_rate_hz = (2.0f * (float) STAGES) / delaySec;
             Ts_bbd = 1.0f / clock_rate_hz;
