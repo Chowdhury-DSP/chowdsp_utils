7c861275060579aa41d3b8c9374a58489a56fc53
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-11-18 16:30:49 +0000

    Adjust wait times for preset manager background thread test

--- tests/gui_tests/PresetsCompTest.cpp
+++ tests/gui_tests/PresetsCompTest.cpp
@@ -175,13 +175,13 @@ public:
 
         std::atomic<bool> threadFinished { false };
         Thread::launch ([&] {
-            Thread::sleep (50); // wait for message manager...
+            Thread::sleep (75); // wait for message manager...
             expectEquals (presetsComp.getPresetMenuText(), String ("Test2"), "Initial preset text is incorrect!");
 
             const auto* menu = presetsComp.getPresetMenuBox().getRootMenu();
             auto menuItem = getMenuItem (*menu, "Test1");
             menuItem->action();
-            Thread::sleep (50); // wait for message manager...
+            Thread::sleep (75); // wait for message manager...
             expectEquals (presetsComp.getPresetMenuText(), String ("Test1"), "Loaded preset text is incorrect!");
 
             threadFinished = true;
