3271a0d5bbab1dc9818471bb3b60ea288f83255d
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-11-21 11:31:11 +0000

    Add plugin logger class

--- modules/chowdsp_plugin_utils/chowdsp_plugin_utils.h
+++ modules/chowdsp_plugin_utils/chowdsp_plugin_utils.h
@@ -47,6 +47,8 @@ JUCE_END_IGNORE_WARNINGS_MSVC
 #include "Presets/chowdsp_Preset.h"
 #include "Presets/chowdsp_PresetManager.h"
 
+#include "Logging/chowdsp_PluginLogger.h"
+
 #include "PluginBase/chowdsp_DummySynthSound.h"
 JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wzero-as-null-pointer-constant", // Clang doesn't like HasAddParameters checker
                                      "-Winconsistent-missing-destructor-override")
