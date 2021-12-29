acbb57ea1ed1e20550d4479da58a73b677b705bc
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-12-29 18:06:40 +0000

    Remove xsimd::select and use select for Decibels methods

--- modules/chowdsp_dsp/SIMD/chowdsp_SIMDDecibels.h
+++ modules/chowdsp_dsp/SIMD/chowdsp_SIMDDecibels.h
@@ -6,17 +6,17 @@ namespace chowdsp::SIMDUtils
 template <typename T>
 inline juce::dsp::SIMDRegister<T> gainToDecibels (const juce::dsp::SIMDRegister<T>& gain, T minusInfinityDB = (T) -100.0)
 {
-    using vec = juce::dsp::SIMDRegister<T>;
-    auto gZero = vec::greaterThan (gain, (T) 0.0);
-    return (vec::max (log10SIMD (gain) * (T) 20.0, minusInfinityDB) & gZero) + ((vec) minusInfinityDB & ~gZero);
+    using Vec = juce::dsp::SIMDRegister<T>;
+    auto gZero = Vec::greaterThan (gain, (T) 0.0);
+    return select (gZero, Vec::max (log10SIMD (gain) * (T) 20.0, minusInfinityDB), (Vec) minusInfinityDB);
 }
 
 /** SIMD specialization of juce::Decibels::decibelsToGain */
 template <typename T>
 inline juce::dsp::SIMDRegister<T> decibelsToGain (const juce::dsp::SIMDRegister<T>& decibels, T minusInfinityDB = (T) -100.0)
 {
-    using vec = juce::dsp::SIMDRegister<T>;
-    auto gZero = vec::greaterThan (decibels, minusInfinityDB);
-    return (powSIMD ((vec) (T) 10.0, decibels * (T) 0.05) & gZero) + ((vec) (T) 0.0 & ~gZero);
+    using Vec = juce::dsp::SIMDRegister<T>;
+    auto gZero = Vec::greaterThan (decibels, minusInfinityDB);
+    return select (gZero, powSIMD ((Vec) (T) 10.0, decibels * (T) 0.05), Vec());
 }
 } // namespace chowdsp::SIMDUtils
--- modules/chowdsp_dsp/SIMD/chowdsp_SIMDLogic.h
+++ modules/chowdsp_dsp/SIMD/chowdsp_SIMDLogic.h
@@ -12,35 +12,11 @@ inline B andnot (B a, B b)
     return a & (~b);
 }
 
-#if CHOWDSP_USE_XSIMD // XSIMD implementations
-template <typename T>
-using x_type = xsimd::batch<T, juce::dsp::SIMDRegister<T>::size()>;
-
-template <typename T>
-using b_type = xsimd::batch_bool<T, juce::dsp::SIMDRegister<T>::size()>;
-
-/** Wrapper of xsimd::select, for ternary logic */
-template <typename T>
-inline juce::dsp::SIMDRegister<T> select (MaskType<T> b, juce::dsp::SIMDRegister<T> t, juce::dsp::SIMDRegister<T> f)
-{
-    return (juce::dsp::SIMDRegister<T>) xsimd::select ((b_type<T>) b.value, (x_type<T>) t.value, (x_type<T>) f.value);
-}
-
-#if (! CHOWDSP_USE_CUSTOM_JUCE_DSP) && (defined(_M_ARM64) || defined(__arm64__) || defined(__aarch64__))
-/** Ternary select operation */
-template <>
-inline juce::dsp::SIMDRegister<double>::vMaskType isnanSIMD (juce::dsp::SIMDRegister<double> x)
-{
-    return (t & b) + (f & ~b);
-}
-#endif
-#else // ! CHOWDSP_USE_XSIMD
 /** Ternary select operation */
 template <typename T>
 inline juce::dsp::SIMDRegister<T> select (MaskType<T> b, juce::dsp::SIMDRegister<T> t, juce::dsp::SIMDRegister<T> f)
 {
     return (t & b) + (f & ~b);
 }
-#endif // CHOWDSP_USE_XSIMD
 
 } // namespace chowdsp::SIMDUtils
