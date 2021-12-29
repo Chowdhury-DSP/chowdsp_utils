f1e02a715f38a08ecc5b08472bba7e5de31b5e24
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-12-29 17:45:59 +0000

    Fix compilation issues for ARM and GCC

--- modules/chowdsp_dsp/SIMD/chowdsp_SIMDLogic.h
+++ modules/chowdsp_dsp/SIMD/chowdsp_SIMDLogic.h
@@ -16,13 +16,25 @@ inline B andnot (B a, B b)
 template <typename T>
 using x_type = xsimd::batch<T, juce::dsp::SIMDRegister<T>::size()>;
 
+template <typename T>
+using b_type = xsimd::batch_bool<T, juce::dsp::SIMDRegister<T>::size()>;
+
 /** Wrapper of xsimd::select, for ternary logic */
 template <typename T>
 inline juce::dsp::SIMDRegister<T> select (MaskType<T> b, juce::dsp::SIMDRegister<T> t, juce::dsp::SIMDRegister<T> f)
 {
-    return (juce::dsp::SIMDRegister<T>) xsimd::select (b.value, (x_type<T>) t.value, (x_type<T>) f.value);
+    return (juce::dsp::SIMDRegister<T>) xsimd::select ((b_type<T>) b.value, (x_type<T>) t.value, (x_type<T>) f.value);
 }
-#else
+
+#if // (! CHOWDSP_USE_CUSTOM_JUCE_DSP) && (defined(_M_ARM64) || defined(__arm64__) || defined(__aarch64__))
+/** Ternary select operation */
+template <>
+inline juce::dsp::SIMDRegister<double>::vMaskType isnanSIMD (juce::dsp::SIMDRegister<double> x)
+{
+    return (t & b) + (f & ~b);
+}
+#endif
+#else // ! CHOWDSP_USE_XSIMD
 /** Ternary select operation */
 template <typename T>
 inline juce::dsp::SIMDRegister<T> select (MaskType<T> b, juce::dsp::SIMDRegister<T> t, juce::dsp::SIMDRegister<T> f)
