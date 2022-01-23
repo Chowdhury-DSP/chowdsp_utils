54498c027cb8971c05818547f10d40cb82902ac0
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-01-23 11:07:21 +0000

    Set up more reliable performance benchmarks (#123)
    * Start setting up real benchmarks with google/benchmark
    
    * Working benchmarks for FloatVectorOperations
    
    * Set up benchmark for comparing performance between IIR and WDFs
    
    * Fix benchmark path
    
    * Apply clang-format
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ bench/bench_utils.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <random>
+#include <vector>
+
+namespace bench_utils
+{
+template <typename T>
+inline auto makeRandomVector (int num)
+{
+    std::random_device rnd_device;
+    std::mt19937 mersenne_engine { rnd_device() }; // Generates random integers
+    std::normal_distribution<float> dist { (T) 0, (T) 1 };
+
+    std::vector<T> vec ((size_t) num);
+    std::generate (vec.begin(), vec.end(), [&dist, &mersenne_engine]() { return dist (mersenne_engine); });
+
+    return std::move (vec);
+}
+} // namespace bench_utils
