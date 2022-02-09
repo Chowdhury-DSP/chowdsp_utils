f1348b8c688be20aeb528f90425c0b8efac282fa
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-02-09 16:38:44 +0000

    Add modified LookupTableTransform for better performance

---
+++ bench/LookupTableBench.cpp
@@ -0,0 +1,48 @@
+#include <benchmark/benchmark.h>
+
+#include <JuceHeader.h>
+#include "bench_utils.h"
+
+constexpr int N = 50000;
+const auto lutVecFloat = bench_utils::makeRandomVector<float> (N);
+auto outVecFloat = std::vector<float> (N, 0.0f);
+const auto juceLUT = juce::dsp::LookupTableTransform<float> ([] (auto x)
+                                                             { return std::tanh (x); },
+                                                             -10.0f,
+                                                             10.0f,
+                                                             1 << 17);
+const auto chowLUT = chowdsp::LookupTableTransform<float> ([] (auto x)
+                                                           { return std::tanh (x); },
+                                                           -10.0f,
+                                                           10.0f,
+                                                           1 << 17);
+
+static void juceTanhLUT (benchmark::State& state)
+{
+    for (auto _ : state)
+        juceLUT.process (lutVecFloat.data(), outVecFloat.data(), N);
+}
+BENCHMARK (juceTanhLUT);
+
+static void chowTanhLUT (benchmark::State& state)
+{
+    for (auto _ : state)
+        chowLUT.process (lutVecFloat.data(), outVecFloat.data(), N);
+}
+BENCHMARK (chowTanhLUT);
+
+static void juceTanhLUTUnchecked (benchmark::State& state)
+{
+    for (auto _ : state)
+        juceLUT.processUnchecked (lutVecFloat.data(), outVecFloat.data(), N);
+}
+BENCHMARK (juceTanhLUTUnchecked);
+
+static void chowTanhLUTUnckecked (benchmark::State& state)
+{
+    for (auto _ : state)
+        chowLUT.processUnchecked (lutVecFloat.data(), outVecFloat.data(), N);
+}
+BENCHMARK (chowTanhLUTUnckecked);
+
+BENCHMARK_MAIN();
--- modules/chowdsp_dsp/chowdsp_dsp.h
+++ modules/chowdsp_dsp/chowdsp_dsp.h
@@ -100,6 +100,7 @@ JUCE_END_IGNORE_WARNINGS_MSVC
 // some useful data structures
 #include "DataStructures/chowdsp_AudioBlockHelpers.h"
 #include "DataStructures/chowdsp_DoubleBuffer.h"
+#include "DataStructures/chowdsp_LookupTableTransform.h"
 #include "DataStructures/chowdsp_RebufferedProcessor.h"
 #include "DataStructures/chowdsp_SmoothedBufferValue.h"
 #include "DataStructures/chowdsp_ScopedValue.h"
