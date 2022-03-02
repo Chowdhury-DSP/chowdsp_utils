15de8e8856b0e1d840f6f85310a68ec1cf0258bc
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-03-02 22:15:32 +0000

    Implement generalized bilinear transform (#138)
    * Add combinatorics methods
    
    * Implement generalized bilinear transform
    
    * Apply clang-format
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ modules/chowdsp_dsp/Math/chowdsp_Combinatorics.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace chowdsp
+{
+/** Implementations of methods commonly used in combinatorics */
+namespace Combinatorics
+{
+    /** Computes the factorial of n using recursion */
+    constexpr int factorial (int x)
+    {
+        return x <= 1 ? 1 : x * factorial (x - 1);
+    }
+
+    /** Computes the permutation of n and k */
+    constexpr int permutation (int n, int k)
+    {
+        return factorial (n) / factorial (n - k);
+    }
+
+    /** Computes the combination of n and k (i.e. "n choose k") */
+    constexpr int combination (int n, int k)
+    {
+        return permutation (n, k) / factorial (k);
+    }
+} // namespace Combinatorics
+} // namespace chowdsp
