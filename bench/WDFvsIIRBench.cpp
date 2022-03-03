ffd2c05e7bb4550bb1682d26ca7871212f67d9c7
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-03-03 11:02:53 +0000

    Fix compiler warnings on MSVC

--- bench/WDFvsIIRBench.cpp
+++ bench/WDFvsIIRBench.cpp
@@ -3,20 +3,23 @@
 #include <JuceHeader.h>
 #include "bench_utils.h"
 
-constexpr auto sampleRate = 48000.0f;
-constexpr int numSamples = 100000;
+namespace
+{
+constexpr auto sampleRateToUse = 48000.0f;
+constexpr int nSamples = 100000;
+} // namespace
 
 static void FirstOrderIIR (benchmark::State& state)
 {
     chowdsp::FirstOrderLPF<float> lpf;
-    lpf.calcCoefs (1000.0f, sampleRate);
+    lpf.calcCoefs (1000.0f, sampleRateToUse);
 
     for (auto _ : state)
     {
         lpf.reset();
 
         float y = 1.0f;
-        for (int i = 0; i < numSamples; ++i)
+        for (int i = 0; i < nSamples; ++i)
             y = lpf.processSample (y);
 
         benchmark::DoNotOptimize (y);
@@ -27,7 +30,7 @@ BENCHMARK (FirstOrderIIR);
 static void FirstOrderWDF (benchmark::State& state)
 {
     namespace wdft = chowdsp::WDFT;
-    wdft::CapacitorT<float> c1 { 1.0e-6f, sampleRate };
+    wdft::CapacitorT<float> c1 { 1.0e-6f, sampleRateToUse };
     wdft::ResistorT<float> r1 { 10.0e3f };
     auto s1 = wdft::makeSeries<float> (c1, r1);
     auto p1 = wdft::makeInverter<float> (s1);
@@ -38,7 +41,7 @@ static void FirstOrderWDF (benchmark::State& state)
         c1.reset();
 
         float y = 1.0f;
-        for (int i = 0; i < numSamples; ++i)
+        for (int i = 0; i < nSamples; ++i)
         {
             vs.setVoltage (y);
             vs.incident (p1.reflected());
@@ -54,14 +57,14 @@ BENCHMARK (FirstOrderWDF);
 static void SecondOrderIIR (benchmark::State& state)
 {
     chowdsp::SecondOrderLPF<float> lpf;
-    lpf.calcCoefs (1000.0f, 0.7071f, sampleRate);
+    lpf.calcCoefs (1000.0f, 0.7071f, sampleRateToUse);
 
     for (auto _ : state)
     {
         lpf.reset();
 
         float y = 1.0f;
-        for (int i = 0; i < numSamples; ++i)
+        for (int i = 0; i < nSamples; ++i)
             y = lpf.processSample (y);
 
         benchmark::DoNotOptimize (y);
@@ -72,8 +75,8 @@ BENCHMARK (SecondOrderIIR);
 static void SecondOrderWDF (benchmark::State& state)
 {
     namespace wdft = chowdsp::WDFT;
-    wdft::CapacitorT<float> c1 { 1.0e-6f, sampleRate };
-    wdft::CapacitorT<float> c2 { 1.0e-6f, sampleRate };
+    wdft::CapacitorT<float> c1 { 1.0e-6f, sampleRateToUse };
+    wdft::CapacitorT<float> c2 { 1.0e-6f, sampleRateToUse };
     wdft::ResistorT<float> r1 { 10.0e3f };
     wdft::ResistorT<float> r2 { 10.0e3f };
     auto s1 = wdft::makeSeries<float> (c1, r1);
@@ -88,7 +91,7 @@ static void SecondOrderWDF (benchmark::State& state)
         c2.reset();
 
         float y = 1.0f;
-        for (int i = 0; i < numSamples; ++i)
+        for (int i = 0; i < nSamples; ++i)
         {
             vs.setVoltage (y);
             vs.incident (p1.reflected());
