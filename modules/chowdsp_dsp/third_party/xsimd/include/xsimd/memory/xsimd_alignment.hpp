aaa561dc40957a26dfe621f3cc58b2ac13f66bee
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-03-29 11:48:34 +0100

    xsimd: update to version 8.0.5

--- modules/chowdsp_dsp/third_party/xsimd/include/xsimd/memory/xsimd_alignment.hpp
+++ modules/chowdsp_dsp/third_party/xsimd/include/xsimd/memory/xsimd_alignment.hpp
@@ -1,33 +1,34 @@
 /***************************************************************************
-* Copyright (c) Johan Mabille, Sylvain Corlay, Wolf Vollprecht and         *
-* Martin Renou                                                             *
-* Copyright (c) QuantStack                                                 *
-*                                                                          *
-* Distributed under the terms of the BSD 3-Clause License.                 *
-*                                                                          *
-* The full license is in the file LICENSE, distributed with this software. *
-****************************************************************************/
+ * Copyright (c) Johan Mabille, Sylvain Corlay, Wolf Vollprecht and         *
+ * Martin Renou                                                             *
+ * Copyright (c) QuantStack                                                 *
+ * Copyright (c) Serge Guelton                                              *
+ *                                                                          *
+ * Distributed under the terms of the BSD 3-Clause License.                 *
+ *                                                                          *
+ * The full license is in the file LICENSE, distributed with this software. *
+ ****************************************************************************/
 
 #ifndef XSIMD_ALIGNMENT_HPP
 #define XSIMD_ALIGNMENT_HPP
 
-#include "../config/xsimd_align.hpp"
+#include "../types/xsimd_utils.hpp"
 #include "xsimd_aligned_allocator.hpp"
 
 namespace xsimd
 {
     /**
-    * @struct aligned_mode
-    * @brief tag for load and store of aligned memory.
-    */
+     * @struct aligned_mode
+     * @brief tag for load and store of aligned memory.
+     */
     struct aligned_mode
     {
     };
 
     /**
-    * @struct unaligned_mode
-    * @brief tag for load and store of unaligned memory.
-    */
+     * @struct unaligned_mode
+     * @brief tag for load and store of unaligned memory.
+     */
     struct unaligned_mode
     {
     };
@@ -42,13 +43,11 @@ namespace xsimd
         using type = unaligned_mode;
     };
 
-#if defined(XSIMD_DEFAULT_ALIGNMENT)
     template <class T>
-    struct allocator_alignment<aligned_allocator<T, XSIMD_DEFAULT_ALIGNMENT>>
+    struct allocator_alignment<aligned_allocator<T>>
     {
         using type = aligned_mode;
     };
-#endif
 
     template <class A>
     using allocator_alignment_t = typename allocator_alignment<A>::type;
@@ -57,15 +56,6 @@ namespace xsimd
      * container alignment *
      ***********************/
 
-    namespace detail
-    {
-        template <class T>
-        struct void_t
-        {
-            using type = void;
-        };
-    }
-
     template <class C, class = void>
     struct container_alignment
     {
@@ -73,7 +63,7 @@ namespace xsimd
     };
 
     template <class C>
-    struct container_alignment<C, typename detail::void_t<typename C::allocator_type>::type>
+    struct container_alignment<C, detail::void_t<typename C::allocator_type>>
     {
         using type = allocator_alignment_t<typename C::allocator_type>;
     };
--- modules/chowdsp_dsp/third_party/xsimd/include/xsimd/xsimd.hpp
+++ modules/chowdsp_dsp/third_party/xsimd/include/xsimd/xsimd.hpp
@@ -1,25 +1,36 @@
 /***************************************************************************
-* Copyright (c) Johan Mabille, Sylvain Corlay, Wolf Vollprecht and         *
-* Martin Renou                                                             *
-* Copyright (c) QuantStack                                                 *
-*                                                                          *
-* Distributed under the terms of the BSD 3-Clause License.                 *
-*                                                                          *
-* The full license is in the file LICENSE, distributed with this software. *
-****************************************************************************/
+ * Copyright (c) Johan Mabille, Sylvain Corlay, Wolf Vollprecht and         *
+ * Martin Renou                                                             *
+ * Copyright (c) QuantStack                                                 *
+ * Copyright (c) Serge Guelton                                              *
+ *                                                                          *
+ * Distributed under the terms of the BSD 3-Clause License.                 *
+ *                                                                          *
+ * The full license is in the file LICENSE, distributed with this software. *
+ ****************************************************************************/
 
 #ifndef XSIMD_HPP
 #define XSIMD_HPP
 
-#include "memory/xsimd_alignment.hpp"
-#include "config/xsimd_config.hpp"
-#include "types/xsimd_traits.hpp"
-#include "math/xsimd_math.hpp"
-#include "math/xsimd_math_complex.hpp"
-#include "memory/xsimd_load_store.hpp"
-
-#include "stl/algorithms.hpp"
-#include "stl/iterator.hpp"
-
+#if defined(__GNUC__)
+#define XSIMD_NO_DISCARD __attribute__((warn_unused_result))
+#else
+#define XSIMD_NO_DISCARD
 #endif
 
+#include "config/xsimd_config.hpp"
+
+#include "arch/xsimd_scalar.hpp"
+#include "memory/xsimd_aligned_allocator.hpp"
+
+#if defined(XSIMD_NO_SUPPORTED_ARCHITECTURE)
+// to type definition or anything appart from scalar definition and aligned allocator
+#else
+#include "types/xsimd_batch.hpp"
+#include "types/xsimd_batch_constant.hpp"
+#include "types/xsimd_traits.hpp"
+
+// This include must come last
+#include "types/xsimd_api.hpp"
+#endif
+#endif
