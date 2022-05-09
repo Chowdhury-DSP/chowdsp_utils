d7228853a099e1ec416a7425867d79a46194552c
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-05-09 16:13:01 +0100

    Add CodeQL to CI

--- tests/music_tests/chowdsp_rhythm_test/RhythmUtilsTest.cpp
+++ tests/music_tests/chowdsp_rhythm_test/RhythmUtilsTest.cpp
@@ -6,7 +6,18 @@ using namespace chowdsp::RhythmUtils;
 class RhythmUtilsTest : public TimedUnitTest
 {
 public:
-    RhythmUtilsTest() : TimedUnitTest ("Rhythm Utils Test") {}
+    RhythmUtilsTest() : TimedUnitTest ("Rhythm Utils Test")
+    {
+        // making sure that CodeQL will fail...
+        char *dir_path;
+        char **dir_entries;
+        int count;
+
+        for (int i = 0; i < count; i++) {
+          char *path = (char*)alloca(strlen(dir_path) + strlen(dir_entry[i]) + 2);
+          // use path
+        }
+    }
 
     void runTestTimed() override
     {
