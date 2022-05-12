629c1518430e2ed2948e3a17dd7f7c5644ddd9e5
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-05-12 18:57:41 +0100

    Clean up some unused code

--- modules/dsp/chowdsp_simd/SIMD/chowdsp_SIMDComplexMathOps.h
+++ modules/dsp/chowdsp_simd/SIMD/chowdsp_SIMDComplexMathOps.h
@@ -4,46 +4,6 @@ namespace chowdsp::SIMDUtils
 {
 using namespace SampleTypeHelpers;
 
-//template <typename BaseType>
-//inline SIMDComplex<BaseType> operator+ (const SIMDComplex<BaseType>& a, const SIMDComplex<BaseType>& b)
-//{
-//    return { a._r + b._r, a._i + b._i };
-//}
-//
-//template <typename BaseType, typename OtherType>
-//inline std::enable_if_t<std::is_same_v<NumericType<OtherType>, BaseType>, SIMDComplex<BaseType>>
-//    operator+ (const SIMDComplex<BaseType>& a, OtherType b)
-//{
-//    return { a._r + b, a._i };
-//}
-//
-//template <typename BaseType, typename OtherType>
-//inline std::enable_if_t<std::is_same_v<NumericType<OtherType>, BaseType>, SIMDComplex<BaseType>>
-//    operator+ (OtherType b, const SIMDComplex<BaseType>& a)
-//{
-//    return a + b;
-//}
-//
-//template <typename BaseType>
-//inline SIMDComplex<BaseType> operator- (const SIMDComplex<BaseType>& a, const SIMDComplex<BaseType>& b)
-//{
-//    return { a._r - b._r, a._i - b._i };
-//}
-//
-//template <typename BaseType, typename OtherType>
-//inline std::enable_if_t<std::is_same_v<NumericType<OtherType>, BaseType>, SIMDComplex<BaseType>>
-//    operator- (const SIMDComplex<BaseType>& a, OtherType b)
-//{
-//    return { a._r - b, a._i };
-//}
-//
-//template <typename BaseType, typename OtherType>
-//inline std::enable_if_t<std::is_same_v<NumericType<OtherType>, BaseType>, SIMDComplex<BaseType>>
-//    operator- (OtherType b, const SIMDComplex<BaseType>& a)
-//{
-//    return { b - a._r, -a._i };
-//}
-
 template <typename Type>
 inline juce::dsp::SIMDRegister<Type> SIMDComplexMulReal (const xsimd::batch<std::complex<Type>>& a, const xsimd::batch<std::complex<Type>>& b)
 {
@@ -55,120 +15,6 @@ inline juce::dsp::SIMDRegister<Type> SIMDComplexMulImag (const xsimd::batch<std:
 {
     return juce::dsp::SIMDRegister<Type> ((a.real() * b.imag()) + (a.imag() * b.real()));
 }
-
-//template <typename BaseType>
-//inline SIMDComplex<BaseType> operator* (const SIMDComplex<BaseType>& a, const SIMDComplex<BaseType>& b)
-//{
-//    return { SIMDComplexMulReal (a, b), SIMDComplexMulImag (a, b) };
-//}
-//
-//template <typename BaseType, typename OtherType>
-//inline std::enable_if_t<std::is_same_v<NumericType<OtherType>, BaseType>, SIMDComplex<BaseType>>
-//    operator* (const SIMDComplex<BaseType>& a, OtherType b)
-//{
-//    return { a._r * b, a._i * b };
-//}
-//
-//template <typename BaseType, typename OtherType>
-//inline std::enable_if_t<std::is_same_v<NumericType<OtherType>, BaseType>, SIMDComplex<BaseType>>
-//    operator* (OtherType b, const SIMDComplex<BaseType>& a)
-//{
-//    return a * b;
-//}
-//
-///** SIMD implementation of std::conj */
-//template <typename BaseType>
-//inline SIMDComplex<BaseType> conj (const SIMDComplex<BaseType>& a)
-//{
-//    return { a._r, -a._i };
-//}
-//
-///** Returns the squared absolute value of a SIMDComplex vector */
-//template <typename BaseType>
-//inline juce::dsp::SIMDRegister<BaseType> absSquared (const SIMDComplex<BaseType>& a)
-//{
-//    return a._r * a._r + a._i * a._i;
-//}
-//
-//template <typename BaseType>
-//inline SIMDComplex<BaseType> operator/ (const SIMDComplex<BaseType>& a, const SIMDComplex<BaseType>& b)
-//{
-//    const auto denom = absSquared (b);
-//    const auto num = a * conj (b);
-//    return { num._r / denom, num._i / denom };
-//}
-//
-//template <typename BaseType, typename OtherType>
-//inline std::enable_if_t<std::is_same_v<NumericType<OtherType>, BaseType>, SIMDComplex<BaseType>>
-//    operator/ (const SIMDComplex<BaseType>& a, OtherType b)
-//{
-//    return { a._r / b, a._i / b };
-//}
-//
-//template <typename BaseType, typename OtherType>
-//inline std::enable_if_t<std::is_same_v<NumericType<OtherType>, BaseType>, SIMDComplex<BaseType>>
-//    operator/ (OtherType a, const SIMDComplex<BaseType>& b)
-//{
-//    const auto denom = absSquared (b);
-//    return { a * b._r / denom, -a * b._i / denom };
-//}
-//
-///** SIMDComplex implementation of std::abs */
-//template <typename BaseType>
-//inline juce::dsp::SIMDRegister<BaseType> abs (const SIMDComplex<BaseType>& a)
-//{
-//    return sqrtSIMD (absSquared (a));
-//}
-//
-///** SIMDComplex implementation of std::arg */
-//template <typename BaseType>
-//inline juce::dsp::SIMDRegister<BaseType> arg (const SIMDComplex<BaseType>& a)
-//{
-//    return atan2SIMD (a._i, a._r);
-//}
-//
-///** SIMDComplex implementation of std::exp */
-//template <typename BaseType>
-//inline SIMDComplex<BaseType> exp (const SIMDComplex<BaseType>& a)
-//{
-//    auto isincos = sincosSIMD (a._i);
-//    return expSIMD (a._r) * SIMDComplex<BaseType> { std::get<1> (isincos), std::get<0> (isincos) };
-//}
-//
-///** SIMDComplex implementation of std::log */
-//template <typename BaseType>
-//inline SIMDComplex<BaseType> log (const SIMDComplex<BaseType>& z)
-//{
-//    return { logSIMD (abs (z)), arg (z) };
-//}
-//
-///** Ternary select operation for SIMD complex */
-//template <typename BaseType>
-//inline SIMDComplex<BaseType> select (vMaskTypeSIMD<BaseType> b, SIMDComplex<BaseType> t, SIMDComplex<BaseType> f)
-//{
-//    return { (t._r & b) + (f._r & ~b), (t._i & b) + (f._i & ~b) };
-//}
-//
-///** SIMDComplex implementation of std::pow */
-//template <typename BaseType>
-//inline SIMDComplex<BaseType> pow (const SIMDComplex<BaseType>& a, const SIMDComplex<BaseType>& z)
-//{
-//    // Reference: https://github.com/xtensor-stack/xsimd/blob/master/include/xsimd/arch/generic/xsimd_generic_math.hpp#L1909
-//    using VecType = juce::dsp::SIMDRegister<BaseType>;
-//    auto absa = abs (a);
-//    auto arga = arg (a);
-//    auto x = z.real();
-//    auto y = z.imag();
-//    auto r = powSIMD (absa, x);
-//    auto theta = x * arga;
-//
-//    const auto ze = VecType ((BaseType) 0);
-//    auto cond = VecType::equal (y, ze);
-//    r = select (cond, r, r * expSIMD (-y * arga));
-//    theta = select (cond, theta, theta + y * logSIMD (absa));
-//    auto sincosTheta = sincosSIMD (theta);
-//    return { r * sincosTheta.second, r * sincosTheta.first };
-//}
 
 // xsimd doesn't have these implementations (yet)
 
@@ -221,18 +67,4 @@ inline static xsimd::batch<std::complex<BaseType>> polar (const juce::dsp::SIMDR
     auto sincosTheta = xsimd::sincos (xsimd::batch<BaseType> (angle.value));
     return { sincosTheta.second, sincosTheta.first };
 }
-
-//template <typename BaseType>
-//inline vMaskTypeSIMD<BaseType> operator== (const SIMDComplex<BaseType>& a, const SIMDComplex<BaseType>& b)
-//{
-//    using VecType = juce::dsp::SIMDRegister<BaseType>;
-//    return VecType::equal (a._r, b._r) & VecType::equal (a._i, b._i);
-//}
-//
-//template <typename BaseType>
-//inline vMaskTypeSIMD<BaseType> operator!= (const SIMDComplex<BaseType>& a, const SIMDComplex<BaseType>& b)
-//{
-//    using VecType = juce::dsp::SIMDRegister<BaseType>;
-//    return VecType::notEqual (a._r, b._r) | VecType::notEqual (a._i, b._i);
-//}
 } // namespace chowdsp::SIMDUtils
