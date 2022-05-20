519a4059bacaf50817f23a3fa9c2f996e5bc31bf
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-05-20 11:25:55 +0100

    Replace BilinearTransforms with more general ConformalMaps

---
+++ modules/dsp/chowdsp_filters/Utils/chowdsp_ConformalMaps.h
@@ -0,0 +1,175 @@
+#pragma once
+
+#include <cmath>
+
+namespace chowdsp
+{
+/**
+ * Utility methods to digitize IIR filters with a conformal map.
+ *
+ * The parameter K an be used to frequency warp the transform or use K = 2 / T
+ * for no warping.
+ */
+namespace ConformalMaps
+{
+    /** Transforms for a (N-1)-order filter */
+    template <typename T, int N>
+    struct Transform
+    {
+        static inline void bilinear (T (&b)[(size_t) N], T (&a)[(size_t) N], const T (&bs)[(size_t) N], const T (&as)[(size_t) N], T K)
+        {
+            using Combinatorics::combination, Power::ipow;
+            constexpr int M = N - 1;
+
+            for (int j = 0; j < N; ++j)
+            {
+                T val_b {};
+                T val_a {};
+                T k_val = (T) 1;
+
+                for (int i = 0; i < N; ++i)
+                {
+                    int n1_pow = 1;
+                    for (int k = 0; k < i + 1; ++k)
+                    {
+                        const auto comb_i_k = combination (i, k);
+                        const auto k_pow = k_val * n1_pow;
+
+                        for (int l = 0; l < N - i; ++l)
+                        {
+                            if (k + l != j)
+                                continue;
+
+                            const auto coeff_mult = comb_i_k * combination (M - i, l) * k_pow;
+                            val_b += coeff_mult * bs[M - i];
+                            val_a += coeff_mult * as[M - i];
+                        }
+
+                        n1_pow *= -1;
+                    }
+
+                    k_val *= K;
+                }
+
+                b[j] = val_b;
+                a[j] = val_a;
+            }
+
+            // normalize coefficients
+            for (int j = N - 1; j >= 0; --j)
+            {
+                b[j] /= a[0];
+                a[j] /= a[0];
+            }
+        }
+    };
+
+    /** Parameters for the Mobius transform. */
+    template <typename T>
+    struct MobiusParams
+    {
+        T a, b, c, d;
+    };
+
+    /** Transforms for a first-order filter */
+    template <typename T>
+    struct Transform<T, 2>
+    {
+        /** First-order Mobius transform */
+        static inline void mobius (T (&b)[2], T (&a)[2], const T (&bs)[2], const T (&as)[2], const MobiusParams<T>& mb)
+        {
+            const auto a0_inv = (T) 1 / (as[0] * mb.a + as[1] * mb.c);
+            b[0] = (bs[0] * mb.a + bs[1] * mb.c) * a0_inv;
+            b[1] = (bs[0] * mb.b + bs[1] * mb.d) * a0_inv;
+            a[0] = (T) 1;
+            a[1] = (as[0] * mb.b + as[1] * mb.d) * a0_inv;
+        }
+
+        /** First-order bilinear transform */
+        static inline void bilinear (T (&b)[2], T (&a)[2], const T (&bs)[2], const T (&as)[2], T K)
+        {
+            mobius (b, a, bs, as, { K, -K, (T) 1, (T) 1 });
+        }
+
+        /** First-order alpha transform */
+        static inline void alpha (T (&b)[2], T (&a)[2], const T (&bs)[2], const T (&as)[2], T K, T alpha, T fs)
+        {
+            const auto Kprime = K / ((T) 2 * fs);
+            const auto alphaA = Kprime * ((T) 1 + alpha) * fs;
+            mobius (b, a, bs, as, { alphaA, -alphaA, (T) 1, alpha });
+        }
+    };
+
+    /** Transforms for a second-order filter */
+    template <typename T>
+    struct Transform<T, 3>
+    {
+        /** Second-order Mobius transform */
+        static inline void mobius (T (&b)[3], T (&a)[3], const T (&bs)[3], const T (&as)[3], const MobiusParams<T>& mb)
+        {
+            const auto mbAsq = mb.a * mb.a;
+            const auto mbAC = mb.a * mb.c;
+            const auto mbCsq = mb.c * mb.c;
+            const auto mb2AB = (T) 2 * mb.a * mb.b;
+            const auto mbAD_BC = mb.a * mb.d + mb.b * mb.c;
+            const auto mb2CD = (T) 2 * mb.c * mb.d;
+            const auto mbBsq = mb.b * mb.b;
+            const auto mbBD = mb.b * mb.d;
+            const auto mbDsq = mb.d * mb.d;
+
+            const auto a0_inv = (T) 1 / (as[0] * mbAsq + as[1] * mbAC + as[2] * mbCsq);
+            b[0] = (bs[0] * mbAsq + bs[1] * mbAC + bs[2] * mbCsq) * a0_inv;
+            b[1] = (bs[0] * mb2AB + bs[1] * mbAD_BC + bs[2] * mb2CD) * a0_inv;
+            b[2] = (bs[0] * mbBsq + bs[1] * mbBD + bs[2] * mbDsq) * a0_inv;
+            a[0] = (T) 1;
+            a[1] = (as[0] * mb2AB + as[1] * mbAD_BC + as[2] * mb2CD) * a0_inv;
+            a[2] = (as[0] * mbBsq + as[1] * mbBD + as[2] * mbDsq) * a0_inv;
+        }
+
+        /** Second-order bilinear transform */
+        static inline void bilinear (T (&b)[3], T (&a)[3], const T (&bs)[3], const T (&as)[3], T K)
+        {
+            // the mobius transform adds a little computational overhead, so here's the optimized bilinear transform.
+            const auto KSq = K * K;
+            const auto a0_inv = (T) 1 / (as[0] * KSq + as[1] * K + as[2]);
+
+            a[0] = (T) 1;
+            a[1] = (T) 2 * (as[2] - as[0] * KSq) * a0_inv;
+            a[2] = (as[0] * KSq - as[1] * K + as[2]) * a0_inv;
+            b[0] = (bs[0] * KSq + bs[1] * K + bs[2]) * a0_inv;
+            b[1] = (T) 2 * (bs[2] - bs[0] * KSq) * a0_inv;
+            b[2] = (bs[0] * KSq - bs[1] * K + bs[2]) * a0_inv;
+        }
+
+        /** Second-order alpha transform */
+        static inline void alpha (T (&b)[3], T (&a)[3], const T (&bs)[3], const T (&as)[3], T K, T alpha, T fs)
+        {
+            const auto Kprime = K / ((T) 2 * fs);
+            const auto alphaA = Kprime * ((T) 1 + alpha) * fs;
+            mobius (b, a, bs, as, { alphaA, -alphaA, (T) 1, alpha });
+        }
+    };
+
+    /** Computes the warping factor "K" so that the frequency fc is matched at sample rate fs */
+    template <typename T>
+    inline T computeKValue (T fc, SampleTypeHelpers::NumericType<T> fs)
+    {
+        using NumericType = SampleTypeHelpers::NumericType<T>;
+        CHOWDSP_USING_XSIMD_STD (tan);
+
+        const auto wc = juce::MathConstants<NumericType>::twoPi * fc;
+        return wc / tan (wc / ((NumericType) 2 * fs));
+    }
+
+    /** Calculates a pole frequency from a set of filter coefficients */
+    template <typename T>
+    inline T calcPoleFreq (T a, T b, T c)
+    {
+        auto radicand = b * b - 4 * a * c;
+        if (radicand >= (T) 0)
+            return (T) 0;
+
+        return std::sqrt (-radicand) / (2 * a);
+    }
+} // namespace ConformalMaps
+} // namespace chowdsp
---
+++ tests/dsp_tests/chowdsp_filters_test/ConformalMapsTest.cpp
@@ -0,0 +1,121 @@
+#include <TimedUnitTest.h>
+#include <chowdsp_filters/chowdsp_filters.h>
+
+JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wsign-conversion")
+
+class ConformalMapsTest : public TimedUnitTest
+{
+public:
+    ConformalMapsTest() : TimedUnitTest ("Conformal Maps Test", "Filters")
+    {
+    }
+
+    template <int N>
+    void testBilinearTransform (const float (&bs)[N], const float (&as)[N], const float (&b_exp)[N], const float (&a_exp)[N], float K)
+    {
+        float b[N] {};
+        float a[N] {};
+        chowdsp::ConformalMaps::Transform<float, N>::bilinear (b, a, bs, as, K);
+
+        for (int i = 0; i < N; ++i)
+        {
+            expectWithinAbsoluteError (b[i], b_exp[i], 1.0e-6f, "B coefficient " + juce::String (i) = " is incorrect!");
+            expectWithinAbsoluteError (a[i], a_exp[i], 1.0e-6f, "A coefficient " + juce::String (i) = " is incorrect!");
+        }
+    }
+
+    void firstOrderAlphaTransformTest()
+    {
+        static constexpr auto fs = 48000.0f;
+        const float bs[2] { 1.0f, 0.0 }; // zero at DC
+        const float as[2] { 1.0f, -1.0e10f }; // pole at very high frequency
+
+        // using the alpha transform as a backwards euler transform
+        {
+            float b[2] {};
+            float a[2] {};
+            chowdsp::ConformalMaps::Transform<float, 2>::alpha (b, a, bs, as, 2.0f * fs, 0.0f, fs);
+
+            expectEquals (b[0], -b[1], "BE should map zero at DC to zero at DC");
+            expectLessThan (std::abs (a[1]), 0.01f, "BE should map high-frequency pole to near zero");
+        }
+
+        // using the alpha transform as a bilinear transform
+        {
+            float b[2] {};
+            float a[2] {};
+            chowdsp::ConformalMaps::Transform<float, 2>::alpha (b, a, bs, as, 2.0f * fs, 1.0f, fs);
+
+            float b_ref[2] {};
+            float a_ref[2] {};
+            chowdsp::ConformalMaps::Transform<float, 2>::bilinear (b_ref, a_ref, bs, as, 2.0f * fs);
+
+            for (int i = 0; i < 2; ++i)
+            {
+                expectWithinAbsoluteError (b[i], b_ref[i], 1.0e-6f, "Bilinear Transform should match reference!");
+                expectWithinAbsoluteError (a[i], a_ref[i], 1.0e-6f, "Bilinear Transform should match reference!");
+            }
+        }
+    }
+
+    void secondOrderAlphaTransformTest()
+    {
+        static constexpr auto fs = 48000.0f;
+        const float bs[3] { 1.0f, 0.0, 0.0f }; // zero at DC
+        const float as[3] { 1.0f, 0.0f, 1.0e10f }; // pole at very high frequency
+
+        // using the alpha transform as a backwards euler transform
+        {
+            float b[3] {};
+            float a[3] {};
+            chowdsp::ConformalMaps::Transform<float, 3>::alpha (b, a, bs, as, 2.0f * fs, 0.0f, fs);
+
+            expectLessThan (std::abs (a[2]), 1.0f, "BE should damp high-frequency pole");
+        }
+
+        // using the alpha transform as a bilinear transform
+        {
+            float b[3] {};
+            float a[3] {};
+            chowdsp::ConformalMaps::Transform<float, 3>::alpha (b, a, bs, as, 2.0f * fs, 1.0f, fs);
+
+            float b_ref[3] {};
+            float a_ref[3] {};
+            chowdsp::ConformalMaps::Transform<float, 3>::bilinear (b_ref, a_ref, bs, as, 2.0f * fs);
+
+            for (int i = 0; i < 3; ++i)
+            {
+                expectWithinAbsoluteError (b[i], b_ref[i], 1.0e-6f, "Bilinear Transform should match reference!");
+                expectWithinAbsoluteError (a[i], a_ref[i], 1.0e-6f, "Bilinear Transform should match reference!");
+            }
+        }
+    }
+
+    void runTestTimed() override
+    {
+        constexpr float fs = 48000.0f;
+        const auto kVal = chowdsp::ConformalMaps::computeKValue (10000.0f, fs);
+
+        beginTest ("First-Order Bilinear Transform");
+        testBilinearTransform ({ 1.0f, 2.0f }, { 2.0f, 3.0f }, { 0.500002623f, -0.499981761f }, { 1.f, -0.999968767f }, 2 * fs);
+        testBilinearTransform ({ 1.0f, 2.0f }, { 2.0f, 3.0f }, { 0.50000304f, -0.499978632f }, { 1.f, -0.999963343f }, kVal);
+
+        beginTest ("Second-Order Bilinear Transform");
+        testBilinearTransform ({ 1.0f, 2.0f, 3.0 }, { 2.0f, 3.0f, 4.0f }, { 0.500002623f, -0.999984323f, 0.499981731f }, { 1.f, -1.99996865f, 0.999968647f }, 2 * fs);
+        testBilinearTransform ({ 1.0f, 2.0f, 3.0 }, { 2.0f, 3.0f, 4.0f }, { 0.50000304f, -0.999981701f, 0.499978632f }, { 1.f, -1.9999634f, 0.999963343f }, kVal);
+
+        beginTest ("Fourth-Order Bilinear Transform");
+        testBilinearTransform ({ 1.0f, 2.0f, 3.0f, 4.0f, 5.0f }, { 2.0f, 3.0f, 4.0f, 5.0f, 6.0f }, { 0.500002623f, -1.99998963f, 2.99995327f, -1.99994779f, 0.499981761f }, { 1.f, -3.99996853f, 5.99990654f, -3.9999063f, 0.999968707f }, 2 * fs);
+        testBilinearTransform ({ 1.0f, 2.0f, 3.0f, 4.0f, 5.0f }, { 2.0f, 3.0f, 4.0f, 5.0f, 6.0f }, { 0.500003099f, -1.99998784f, 2.99994516f, -1.99993896f, 0.499978632f }, { 1.f, -3.99996352f, 5.99989032f, -3.99989009f, 0.999963402f }, kVal);
+
+        beginTest ("First-Order Alpha Transform");
+        firstOrderAlphaTransformTest();
+
+        beginTest ("Second-Order Alpha Transform");
+        secondOrderAlphaTransformTest();
+    }
+};
+
+JUCE_END_IGNORE_WARNINGS_GCC_LIKE
+
+static ConformalMapsTest conformalMapsTest;
