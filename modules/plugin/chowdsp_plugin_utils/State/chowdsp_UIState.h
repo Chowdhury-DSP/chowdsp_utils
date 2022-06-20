1f30c4eb648c87e268715be0c198b1a29c099368
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-06-20 11:53:39 +0100

    UIState: make a few things protected instead of private (#228)

--- modules/plugin/chowdsp_plugin_utils/State/chowdsp_UIState.h
+++ modules/plugin/chowdsp_plugin_utils/State/chowdsp_UIState.h
@@ -10,17 +10,19 @@ public:
     explicit UIState (juce::AudioProcessorValueTreeState& vts, int defaultWidth = 400, int defaultHeight = 400);
 
     /** Attaches the state to the plugin editor */
-    void attachToComponent (juce::AudioProcessorEditor& comp);
+    virtual void attachToComponent (juce::AudioProcessorEditor& comp);
 
     /** Returns the previous editor size */
     [[nodiscard]] juce::Rectangle<int> getLastEditorSize() const;
 
-private:
+protected:
     void componentBeingDeleted (juce::Component& component) override;
     void componentMovedOrResized (juce::Component& component, bool wasMoved, bool wasResized) override;
-    void setLastEditorSize (const juce::Rectangle<int>& bounds);
 
     juce::AudioProcessorValueTreeState& vts;
+
+private:
+    void setLastEditorSize (const juce::Rectangle<int>& bounds);
 
     const int defaultWidth;
     const int defaultHeight;
