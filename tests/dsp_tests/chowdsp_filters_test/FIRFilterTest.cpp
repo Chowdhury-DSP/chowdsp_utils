4a270bd40e103eba1ccae37087f8be6759e55d5b
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-06-23 19:58:53 +0100

    FIRFilter: improvements and fixes (#230)
    * FIRFilter improvements and fixes
    
    * Add tests for FIR filter
    
    * Apply clang-format
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- tests/dsp_tests/chowdsp_filters_test/FIRFilterTest.cpp
+++ tests/dsp_tests/chowdsp_filters_test/FIRFilterTest.cpp
@@ -49,12 +49,66 @@ public:
         }
     }
 
+    void firFilterBypassTest (juce::Random& rand)
+    {
+        static constexpr double fs = 48000.0;
+        static constexpr int blockSize = 512;
+        static constexpr int firOrder = 63;
+
+        auto&& coeffsBuffer = test_utils::makeNoise (rand, firOrder);
+        auto&& refBuffer = test_utils::makeSineWave (100.0f, (float) fs, blockSize);
+        auto&& actualBuffer = juce::AudioBuffer { refBuffer };
+
+        {
+            juce::dsp::FIR::Filter<float> refFilter;
+            auto& coefs = refFilter.coefficients->coefficients;
+            coefs.resize (firOrder);
+            auto* coeffsBufferData = coeffsBuffer.getReadPointer (0);
+            std::copy (coeffsBufferData, coeffsBufferData + firOrder, coefs.begin());
+
+            refFilter.prepare ({ fs, (juce::uint32) blockSize, 1 });
+
+            auto&& copyBuffer = juce::AudioBuffer<float> { refBuffer };
+            auto&& copyBlock = juce::dsp::AudioBlock<float> { copyBuffer };
+            auto&& refBlock = juce::dsp::AudioBlock<float> { refBuffer };
+            refFilter.process (juce::dsp::ProcessContextReplacing<float> { copyBlock });
+            refFilter.process (juce::dsp::ProcessContextReplacing<float> { refBlock });
+        }
+
+        {
+            chowdsp::FIRFilter<float> filter;
+            filter.setOrder (firOrder);
+            filter.setCoefficients (coeffsBuffer.getReadPointer (0));
+            expectEquals (filter.getOrder(), firOrder, "Filter order is incorrect!");
+
+            auto&& copyBuffer = juce::AudioBuffer<float> { refBuffer };
+            auto&& copyBlock = juce::dsp::AudioBlock<float> { copyBuffer };
+            auto&& block = juce::dsp::AudioBlock<float> { actualBuffer };
+
+            filter.processBlock (copyBlock);
+            filter.reset();
+
+            filter.processBlockBypassed (block);
+            filter.processBlock (block);
+        }
+
+        for (int i = 0; i < blockSize; ++i)
+        {
+            const auto ref = refBuffer.getSample (0, i);
+            const auto actual = actualBuffer.getSample (0, i);
+            expectWithinAbsoluteError (actual, ref, 1.0e-3f, "Sample at index: " + juce::String (i) + " is incorrect!");
+        }
+    }
+
     void runTestTimed() override
     {
         auto random = getRandom();
 
         beginTest ("FIR Filter Test");
         firFilterTest (random);
+
+        beginTest ("FIR Filter Bypass Test");
+        firFilterBypassTest (random);
     }
 };
 
