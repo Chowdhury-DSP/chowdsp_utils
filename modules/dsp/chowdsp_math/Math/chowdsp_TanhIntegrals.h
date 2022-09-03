eb49beb4535956282d5e1fbe187fb5999db871a1
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-09-03 10:59:07 -0700

    Fixing flaky waveshapers test on Windows

--- modules/dsp/chowdsp_math/Math/chowdsp_TanhIntegrals.h
+++ modules/dsp/chowdsp_math/Math/chowdsp_TanhIntegrals.h
@@ -22,7 +22,7 @@ namespace TanhIntegrals
     inline T tanhAD2 (T x) noexcept
     {
         using Polylogarithm::Li2, Power::ipow;
-        const auto expVal = gcem::exp ((T) -2 * x);
+        const auto expVal = std::exp ((T) -2 * x);
         return (T) 0.5 * (Li2 (-expVal) - x * (x + (T) 2 * std::log (expVal + (T) 1) - (T) 2 * std::log (std::cosh (x)))) + (ipow<2> (juce::MathConstants<T>::pi) / (T) 24);
     }
 } // namespace TanhIntegrals
