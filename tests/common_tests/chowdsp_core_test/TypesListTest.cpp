3ddf99e51fb5aa2517441d236de2d9cafe5d2355
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-10-05 23:27:39 -0700

    Add TypesList (#309)
    * Add TypesList
    
    * Add test for types list
    
    * Apply clang-format
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ tests/common_tests/chowdsp_core_test/TypesListTest.cpp
@@ -0,0 +1,42 @@
+#include <CatchUtils.h>
+#include <chowdsp_core/chowdsp_core.h>
+#include <array>
+
+struct Type0
+{
+    static constexpr std::string_view name = "type0";
+};
+
+struct Type1
+{
+    static constexpr std::string_view name = "type1";
+};
+
+struct Type2
+{
+    static constexpr std::string_view name = "type2";
+};
+
+TEST_CASE ("Types List Test")
+{
+    SECTION ("For Each In Type List Test")
+    {
+        using List = chowdsp::TypesList<
+            Type0,
+            Type1,
+            Type2>;
+
+        static constexpr auto typeNames = [] {
+            std::array<std::string_view, List::count> names {};
+            chowdsp::ForEachInTypeList<List>::doForEach (
+                [&names] (auto index) {
+                    names[index] = List::AtIndex<index>::name;
+                });
+            return names;
+        }();
+
+        static_assert (typeNames[0] == "type0");
+        static_assert (typeNames[1] == "type1");
+        static_assert (typeNames[2] == "type2");
+    }
+}
