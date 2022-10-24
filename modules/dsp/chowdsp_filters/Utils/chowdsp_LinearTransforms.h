4b279d5b71c275f9131e6e21a433d82652c435ea
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-10-23 22:46:45 -0700

    Minor tweaks

--- modules/dsp/chowdsp_filters/Utils/chowdsp_LinearTransforms.h
+++ modules/dsp/chowdsp_filters/Utils/chowdsp_LinearTransforms.h
@@ -5,9 +5,9 @@ namespace chowdsp
 /** Transforming filters with some linear transformations */
 namespace LinearTransforms
 {
-    /** Transforms a filter with some negative feedback. */
+    /** Transforms a filter with some amount of feedback. */
     template <int Order, typename T, bool normalize = true>
-    inline void transformNegativeFeedback (T (&b_coeffs)[Order + 1], T (&a_coeffs)[Order + 1], T G)
+    inline void transformFeedback (T (&b_coeffs)[Order + 1], T (&a_coeffs)[Order + 1], T G)
     {
         const auto a0 = a_coeffs[0] - G * b_coeffs[0];
         const auto a0_inv = normalize ? (T) 1 / a0 : (T) 1;
