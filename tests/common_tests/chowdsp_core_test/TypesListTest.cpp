8cb6a4315ef8369f5bbad2aff8d6019050c11c17
jatin <jatinchowdhury18@gmail.com>
2022-11-02 11:32:42 -0700

    Improvements to TypesList

--- tests/common_tests/chowdsp_core_test/TypesListTest.cpp
+++ tests/common_tests/chowdsp_core_test/TypesListTest.cpp
@@ -26,10 +26,12 @@ TEST_CASE ("Types List Test")
             Type1,
             Type2>;
 
-        static constexpr auto typeNames = [] {
+        static constexpr auto typeNames = []
+        {
             std::array<std::string_view, List::count> names {};
             chowdsp::ForEachInTypeList<List>::doForEach (
-                [&names] (auto index) {
+                [&names] (auto index)
+                {
                     names[index] = List::AtIndex<index>::name;
                 });
             return names;
@@ -44,5 +46,30 @@ TEST_CASE ("Types List Test")
     {
         static_assert (chowdsp::TypesList<std::string, int, float>::contains<int>);
         static_assert (! chowdsp::TypesList<std::string, int, float>::contains<double>);
+    }
+
+    SECTION ("Insert Front Test")
+    {
+        using BaseList = chowdsp::TypesList<Type0>;
+        static_assert (std::is_same_v<chowdsp::TypesList<Type1, Type0>, BaseList::InsertFront<Type1>>);
+        static_assert (std::is_same_v<chowdsp::TypesList<Type1, Type2, Type0>, BaseList::InsertFront<Type1, Type2>>);
+    }
+
+    SECTION ("Insert Back Test")
+    {
+        using BaseList = chowdsp::TypesList<Type0>;
+        static_assert (std::is_same_v<chowdsp::TypesList<Type0, Type1>, BaseList::InsertBack<Type1>>);
+        static_assert (std::is_same_v<chowdsp::TypesList<Type0, Type1, Type2>, BaseList::InsertBack<Type1, Type2>>);
+    }
+
+    SECTION ("Concatenate Test")
+    {
+        static_assert (std::is_same_v<chowdsp::TypesList<int, float>, chowdsp::TypesList<int>::Concatenate<chowdsp::TypesList<float>>>);
+    }
+
+    SECTION ("Tuple Test")
+    {
+        static_assert (std::is_same_v<chowdsp::TypesList<int, float>, chowdsp::TupleTypeList<std::tuple<int, float>>>);
+        static_assert (std::is_same_v<chowdsp::TypesList<int, float>::Types, std::tuple<int, float>>);
     }
 }
