06339fcb51d9eef959506cf309a02ec34a1ea1f0
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-11-22 10:17:33 -0800

    SynthBase: Implement CLAP note names (#336)
    * SynthBase: Implement CLAP note names
    
    * Apply clang-format
    
    * Fix clang-tidy error
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/plugin/chowdsp_plugin_base/PluginBase/chowdsp_SynthBase.h
+++ modules/plugin/chowdsp_plugin_base/PluginBase/chowdsp_SynthBase.h
@@ -36,6 +36,15 @@ public:
         processSynth (buffer, midi);
     }
 
+#if JUCE_MODULE_AVAILABLE_chowdsp_clap_extensions
+    [[nodiscard]] bool supportsNoteName() const noexcept override
+    {
+        return true;
+    }
+    int noteNameCount() noexcept override { return 0; }
+    bool noteNameGet (int /*index*/, clap_note_name* /*noteName*/) noexcept override { return false; }
+#endif
+
 private:
     static juce::AudioProcessor::BusesProperties getDefaultBusLayout()
     {
