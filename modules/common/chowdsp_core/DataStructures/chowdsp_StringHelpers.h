0c649d805ce780f758de428cc32dd1f45bfca46a
jatin <jatinchowdhury18@gmail.com>
2022-12-07 17:53:18 -0600

    Add helper for concatenating string_views

--- modules/common/chowdsp_core/DataStructures/chowdsp_StringHelpers.h
+++ modules/common/chowdsp_core/DataStructures/chowdsp_StringHelpers.h
@@ -9,4 +9,37 @@ inline juce::String toString (const std::string_view& sv) noexcept
     return juce::String { sv.data(), sv.size() };
 }
 #endif
+
+#ifndef DOXYGEN
+namespace string_helpers_detail
+{
+    // Implementation details borrowed from: https://stackoverflow.com/questions/38955940/how-to-concatenate-static-strings-at-compile-time
+    template <const std::string_view&... Strs>
+    struct concat
+    {
+        // Join all strings into a single std::array of chars
+        static constexpr auto impl() noexcept
+        {
+            constexpr size_t len = (Strs.size() + ... + 0);
+            std::array<char, len + 1> arr_internal {};
+            auto append = [i = (size_t) 0, &arr_internal] (auto const& s) mutable
+            {
+                for (auto c : s)
+                    arr_internal[i++] = c;
+            };
+            (append (Strs), ...);
+            arr_internal[len] = 0;
+            return arr_internal;
+        }
+        // Give the joined string static storage
+        static constexpr auto arr = impl();
+        // View as a std::string_view
+        static constexpr std::string_view value { arr.data(), arr.size() - 1 };
+    };
+} // namespace string_helpers_detail
+#endif // DOXYGEN
+
+/** Concatenates a group of string_views */
+template <std::string_view const&... Strs>
+static constexpr auto string_view_concat = string_helpers_detail::concat<Strs...>::value;
 } // namespace chowdsp
