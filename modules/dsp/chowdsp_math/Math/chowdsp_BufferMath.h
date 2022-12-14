e25381e8491e8927da3894a99483107a42953e3f
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-12-13 18:02:21 -0600

    BufferMath: add implementations for out-of-place operations (#355)
    * Starting to improve BufferMath interface
    
    * BufferMath: implementing out-of-place functions
    
    * Remove un-needed forward declaration

--- modules/dsp/chowdsp_math/Math/chowdsp_BufferMath.h
+++ modules/dsp/chowdsp_math/Math/chowdsp_BufferMath.h
@@ -62,13 +62,25 @@ namespace BufferMath
     template <typename BufferType, typename FloatType = typename BufferType::Type>
     void applyGain (BufferType& buffer, FloatType gain) noexcept;
 
+    /** Applies a linear gain to a buffer and stores the result in another buffer. */
+    template <typename BufferType1, typename BufferType2 = BufferType1, typename FloatType = typename BufferType1::Type>
+    void applyGain (const BufferType1& bufferSrc, BufferType2& bufferDest, FloatType gain) noexcept;
+
     /** Applies a linear gain to a buffer. */
     template <typename BufferType, typename SmoothedValueType>
     void applyGainSmoothed (BufferType& buffer, SmoothedValueType& gain) noexcept;
 
+    /** Applies a linear gain to a buffer and stores the result in another buffer. */
+    template <typename BufferType1, typename SmoothedValueType, typename BufferType2 = BufferType1>
+    void applyGainSmoothed (const BufferType1& bufferSrc, BufferType2& bufferDest, SmoothedValueType& gain) noexcept;
+
     /** Applies a linear gain to a buffer. */
     template <typename BufferType, typename SmoothedBufferType>
     void applyGainSmoothedBuffer (BufferType& buffer, SmoothedBufferType& gain) noexcept;
+
+    /** Applies a linear gain to a buffer and stores the result in another buffer. */
+    template <typename BufferType1, typename SmoothedBufferType, typename BufferType2 = BufferType1>
+    void applyGainSmoothedBuffer (const BufferType1& bufferSrc, BufferType2& bufferDest, SmoothedBufferType& gain) noexcept;
 
     /**
      * If the buffer contains any Infs, NaNs, or values larger than the ceiling,
@@ -77,18 +89,33 @@ namespace BufferMath
     template <typename BufferType, typename FloatType = detail::BufferSampleType<BufferType>>
     bool sanitizeBuffer (BufferType& buffer, FloatType ceiling = (FloatType) 100) noexcept;
 
-    /** Applies a function to every sample in the buffer */
+    /** Applies a function to every sample in the buffer. */
     template <typename BufferType, typename FunctionType>
-    void applyFunction (BufferType& buffer, FunctionType&& function);
+    void applyFunction (BufferType& buffer, FunctionType&& function) noexcept;
+
+    /** Applies a function to every sample in the buffer and stores the result in another buffer. */
+    template <typename BufferType1, typename BufferType2, typename FunctionType>
+    void applyFunction (const BufferType1& bufferSrc, BufferType2& bufferDest, FunctionType&& function) noexcept;
 
 #if ! CHOWDSP_NO_XSIMD
-    /** Applies a SIMD/scalar function to every sample in the buffer */
+    /** Applies a SIMD/scalar function to every sample in the buffer. */
     template <typename BufferType, typename FunctionType, typename FloatType = detail::BufferSampleType<BufferType>>
-    std::enable_if_t<std::is_floating_point_v<FloatType>, void> applyFunctionSIMD (BufferType& buffer, FunctionType&& function);
+    std::enable_if_t<std::is_floating_point_v<FloatType>, void> applyFunctionSIMD (BufferType& buffer, FunctionType&& function) noexcept;
 
-    /** Applies a SIMD or scalar function to every sample in the buffer */
+    /** Applies a SIMD/scalar function to every sample in the buffer and stores the result in another buffer. */
+    template <typename BufferType1, typename BufferType2, typename FunctionType, typename FloatType = detail::BufferSampleType<BufferType1>>
+    std::enable_if_t<std::is_floating_point_v<FloatType>, void>
+        applyFunctionSIMD (const BufferType1& bufferSrc, BufferType2& bufferDest, FunctionType&& function) noexcept;
+
+    /** Applies a SIMD or scalar function to every sample in the buffer. */
     template <typename BufferType, typename SIMDFunctionType, typename ScalarFunctionType, typename FloatType = detail::BufferSampleType<BufferType>>
-    std::enable_if_t<std::is_floating_point_v<FloatType>, void> applyFunctionSIMD (BufferType& buffer, SIMDFunctionType&& simdFunction, ScalarFunctionType&& scalarFunction);
+    std::enable_if_t<std::is_floating_point_v<FloatType>, void>
+        applyFunctionSIMD (BufferType& buffer, SIMDFunctionType&& simdFunction, ScalarFunctionType&& scalarFunction) noexcept;
+
+    /** Applies a SIMD or scalar function to every sample in the buffer and stores the result in another buffer. */
+    template <typename BufferType1, typename BufferType2, typename SIMDFunctionType, typename ScalarFunctionType, typename FloatType = detail::BufferSampleType<BufferType1>>
+    std::enable_if_t<std::is_floating_point_v<FloatType>, void>
+        applyFunctionSIMD (const BufferType1& bufferSrc, BufferType2& bufferDest, SIMDFunctionType&& simdFunction, ScalarFunctionType&& scalarFunction) noexcept;
 #endif
 } // namespace BufferMath
 } // namespace chowdsp
