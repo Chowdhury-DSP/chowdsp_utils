6fccba9370c2fc63252344d5f16fc0cf201aab9e
jatin <jatinchowdhury18@gmail.com>
2023-02-02 21:32:23 -0800

    Add documentation

--- modules/gui/chowdsp_visualizers/SpectrumPlots/chowdsp_GenericFilterPlotter.h
+++ modules/gui/chowdsp_visualizers/SpectrumPlots/chowdsp_GenericFilterPlotter.h
@@ -2,9 +2,17 @@
 
 namespace chowdsp
 {
+/**
+ * Helper class for plotting the frequency response of some generic filter.
+ *
+ * The class generates the plot as a juce::Path, by passing a sine sweep
+ * through the filter (supplied by the user via a std::function), and then
+ * computing the frequency response with partial-octave-band smoothing.
+ */
 class GenericFilterPlotter
 {
 public:
+    /** Parameters for the plot. */
     struct Params
     {
         float sampleRate = 48000.0f;
@@ -12,13 +20,19 @@ public:
         int fftOrder = 13;
     };
 
+    /** Constructs a plotter from a SpectrumPlotBase */
     explicit GenericFilterPlotter (const SpectrumPlotBase& plotBase, Params&& plotParams);
 
+    /** Runs data through the filter process and returns the plot as a pair of frequency/magnitude vectors */
     std::pair<const std::vector<float>&, const std::vector<float>&> plotFilterMagnitudeResponse();
+
+    /** Updates the internal juce::Path with a new frequency response */
     void updateFilterPlot();
 
+    /** Returns the current path. */
     [[nodiscard]] const auto& getPath() const { return plotPath; }
 
+    /** Users should implement this function to perform the filtering process. */
     std::function<void(const float*, float*, int)> runFilterCallback;
 
     const Params params;
