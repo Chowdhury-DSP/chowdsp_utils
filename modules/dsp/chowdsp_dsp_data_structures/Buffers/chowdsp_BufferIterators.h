be000afdfe33f319d79aa17b125f514b057fff85
jatin <jatinchowdhury18@gmail.com>
2023-02-16 15:31:54 -0800

    buffer_iters: add iterator for buffer in sub-blocks

--- modules/dsp/chowdsp_dsp_data_structures/Buffers/chowdsp_BufferIterators.h
+++ modules/dsp/chowdsp_dsp_data_structures/Buffers/chowdsp_BufferIterators.h
@@ -146,6 +146,107 @@ namespace buffer_iters
         };
         return iterable_wrapper { buffer };
     }
+
+    /**
+     * Iterates over a buffer in sub-blocks.
+     *
+     * @tparam subBlockSize The iterator will always supply sub-blocks of this size _or smaller_.
+     * @tparam channelWise  If true, the iterator will iterate over the buffer channels first.
+     */
+    template <int subBlockSize, bool channelWise = false, typename BufferType>
+    constexpr auto sub_blocks (BufferType& buffer)
+    {
+        struct iterator
+        {
+            BufferType& buffer;
+            int samplesRemaining;
+            int channelIndex;
+            bool operator!= (const iterator& other) const
+            {
+                if constexpr (channelWise)
+                    return &buffer != &other.buffer || samplesRemaining != other.samplesRemaining;
+                else
+                    return &buffer != &other.buffer || channelIndex != other.channelIndex;
+            }
+
+            void operator++()
+            {
+                if constexpr (channelWise)
+                {
+                    ++channelIndex;
+                    if (channelIndex == buffer.getNumChannels())
+                    {
+                        samplesRemaining = juce::jmax (samplesRemaining - subBlockSize, 0);
+                        if (samplesRemaining > 0)
+                            channelIndex = 0;
+                    }
+                }
+                else
+                {
+                    samplesRemaining = juce::jmax (samplesRemaining - subBlockSize, 0);
+                    if (samplesRemaining == 0)
+                    {
+                        ++channelIndex;
+                        if (channelIndex < buffer.getNumChannels())
+                            samplesRemaining = buffer.getNumSamples();
+                    }
+                }
+            }
+
+            auto operator*() const
+            {
+                const auto startSample = buffer.getNumSamples() - samplesRemaining;
+                const auto activeSubBlockSize = (size_t) juce::jmin (subBlockSize, samplesRemaining);
+                if constexpr (IsConstBufferType<BufferType>)
+                {
+#if CHOWDSP_USING_JUCE
+                    if constexpr (std::is_same_v<BufferType, const juce::AudioBuffer<float>> || std::is_same_v<BufferType, const juce::AudioBuffer<double>>)
+                    {
+                        return std::make_tuple (channelIndex,
+                                                startSample,
+                                                nonstd::span {
+                                                    buffer.getReadPointer (channelIndex) + startSample,
+                                                    activeSubBlockSize });
+                    }
+                    else
+#endif
+                    {
+                        return std::make_tuple (channelIndex,
+                                                startSample,
+                                                buffer.getReadSpan (channelIndex)
+                                                    .subspan ((size_t) startSample, activeSubBlockSize));
+                    }
+                }
+                else
+                {
+#if CHOWDSP_USING_JUCE
+                    if constexpr (std::is_same_v<BufferType, juce::AudioBuffer<float>> || std::is_same_v<BufferType, juce::AudioBuffer<double>>)
+                    {
+                        return std::make_tuple (channelIndex,
+                                                startSample,
+                                                nonstd::span {
+                                                    buffer.getWritePointer (channelIndex) + startSample,
+                                                    activeSubBlockSize });
+                    }
+                    else
+#endif
+                    {
+                        return std::make_tuple (channelIndex,
+                                                startSample,
+                                                buffer.getWriteSpan (channelIndex)
+                                                    .subspan ((size_t) startSample, activeSubBlockSize));
+                    }
+                }
+            }
+        };
+        struct iterable_wrapper
+        {
+            BufferType& buffer;
+            auto begin() { return iterator { buffer, buffer.getNumSamples(), 0 }; }
+            auto end() { return iterator { buffer, 0, buffer.getNumChannels() }; }
+        };
+        return iterable_wrapper { buffer };
+    }
 } // namespace buffer_iters
 
 } // namespace chowdsp
