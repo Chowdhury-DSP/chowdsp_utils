7176ce8158b5315e7058e1c812f0d301733cb595
jatin <jatinchowdhury18@gmail.com>
2023-02-16 15:48:27 -0800

    More buffer iter tests

--- tests/dsp_tests/chowdsp_dsp_juce_test/data_structures_tests/BufferIteratorsTest.cpp
+++ tests/dsp_tests/chowdsp_dsp_juce_test/data_structures_tests/BufferIteratorsTest.cpp
@@ -19,31 +19,89 @@ TEMPLATE_TEST_CASE ("Buffer Iterators Test",
                     (chowdsp::Buffer<double>),
                     (chowdsp::Buffer<xsimd::batch<float>>),
                     (chowdsp::Buffer<xsimd::batch<double>>),
-                    (chowdsp::StaticBuffer<float, 2, 8>),
-                    (chowdsp::StaticBuffer<double, 2, 8>),
-                    (chowdsp::StaticBuffer<xsimd::batch<float>, 2, 8>),
-                    (chowdsp::StaticBuffer<xsimd::batch<double>, 2, 8>),
+                    (chowdsp::StaticBuffer<float, 2, 96>),
+                    (chowdsp::StaticBuffer<double, 2, 96>),
+                    (chowdsp::StaticBuffer<xsimd::batch<float>, 2, 96>),
+                    (chowdsp::StaticBuffer<xsimd::batch<double>, 2, 96>),
                     (juce::AudioBuffer<float>),
                     (juce::AudioBuffer<double>) )
 {
     using BufferType = TestType;
     using SampleType = chowdsp::BufferMath::detail::BufferSampleType<BufferType>;
 
-    BufferType buffer { 2, 4 };
-
+    SECTION ("Channels")
     {
+        BufferType buffer { 2, 4 };
+
+        {
+            int count = 0;
+            for (auto [channel, data] : chowdsp::buffer_iters::channels (buffer))
+            {
+                for (auto& x_n : data)
+                    x_n = (SampleType) static_cast<float> (count++);
+            }
+        }
+
         int count = 0;
-        for (auto [channel, data] : chowdsp::buffer_iters::channels (buffer))
+        for (auto [channel, data] : chowdsp::buffer_iters::channels (chowdsp::asConstBuffer (buffer)))
         {
             for (auto& x_n : data)
-                x_n = (SampleType) static_cast<float> (count++);
+                REQUIRE (chowdsp::SIMDUtils::all ((SampleType) static_cast<float> (count++) == x_n));
         }
     }
 
-    int count = 0;
-    for (auto [channel, data] : chowdsp::buffer_iters::channels (chowdsp::asConstBuffer (buffer)))
+    struct SubBlocksTester
     {
-        for (auto& x_n : data)
-            REQUIRE (chowdsp::SIMDUtils::all ((SampleType) static_cast<float> (count++) == x_n));
+        int channel;
+        int sample;
+        int size;
+        const SampleType* data;
+    };
+
+    const auto testBufferSubBlocks = [] (auto& b, const auto& testers, const auto channelWise)
+    {
+        size_t test_count = 0;
+        for (auto [ch, n, sub_block] : chowdsp::buffer_iters::sub_blocks<32, channelWise> (b))
+        {
+            REQUIRE (ch == testers[test_count].channel);
+            REQUIRE (n == testers[test_count].sample);
+            REQUIRE ((int) sub_block.size() == testers[test_count].size);
+            REQUIRE (sub_block.data() == testers[test_count].data);
+            ++test_count;
+        }
+    };
+
+    SECTION ("Sub-Blocks (Sample-wise)")
+    {
+        BufferType buffer { 2, 70 };
+
+        const std::array<SubBlocksTester, 6> testers {
+            SubBlocksTester {0, 0, 32, buffer.getWritePointer (0)},
+            SubBlocksTester {0, 32, 32, buffer.getWritePointer (0) + 32},
+            SubBlocksTester {0, 64, 6, buffer.getWritePointer (0) + 64},
+            SubBlocksTester {1, 0, 32, buffer.getWritePointer (1)},
+            SubBlocksTester {1, 32, 32, buffer.getWritePointer (1) + 32},
+            SubBlocksTester {1, 64, 6, buffer.getWritePointer (1) + 64},
+        };
+
+        testBufferSubBlocks (buffer, testers, std::integral_constant<bool, false>{});
+        testBufferSubBlocks (chowdsp::asConstBuffer (buffer), testers, std::integral_constant<bool, false>{});
+    }
+
+    SECTION ("Sub-Blocks (Channel-wise)")
+    {
+        BufferType buffer { 2, 70 };
+
+        const std::array<SubBlocksTester, 6> testers {
+            SubBlocksTester {0, 0, 32, buffer.getWritePointer (0)},
+            SubBlocksTester {1, 0, 32, buffer.getWritePointer (1)},
+            SubBlocksTester {0, 32, 32, buffer.getWritePointer (0) + 32},
+            SubBlocksTester {1, 32, 32, buffer.getWritePointer (1) + 32},
+            SubBlocksTester {0, 64, 6, buffer.getWritePointer (0) + 64},
+            SubBlocksTester {1, 64, 6, buffer.getWritePointer (1) + 64},
+        };
+
+        testBufferSubBlocks (buffer, testers, std::integral_constant<bool, true>{});
+        testBufferSubBlocks (chowdsp::asConstBuffer (buffer), testers, std::integral_constant<bool, true>{});
     }
 }
