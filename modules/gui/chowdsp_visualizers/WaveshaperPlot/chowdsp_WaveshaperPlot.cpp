c149b67c4a46eef0608856a18dca0e775db3ded8
jatin <jatinchowdhury18@gmail.com>
2023-03-08 19:52:52 -0800

    Add WaveshaperPlot path

---
+++ modules/gui/chowdsp_visualizers/WaveshaperPlot/chowdsp_WaveshaperPlot.cpp
@@ -0,0 +1,52 @@
+namespace chowdsp
+{
+WaveshaperPlot::WaveshaperPlot (WaveshaperPlotParams&& plotParams) : params (plotParams) {}
+
+float WaveshaperPlot::getXCoordinateForAmplitude (float amplitude) const noexcept
+{
+    amplitude = juce::jlimit (params.xMin, params.xMax, amplitude);
+    return juce::jmap (amplitude, params.xMin, params.xMax, 0.0f, (float) bounds.getWidth());
+}
+
+float WaveshaperPlot::getYCoordinateForAmplitude (float amplitude) const noexcept
+{
+    amplitude = juce::jlimit (params.yMin, params.yMax, amplitude);
+    return juce::jmap (amplitude, params.yMin, params.yMax, (float) bounds.getHeight(), 0.0f);
+}
+
+void WaveshaperPlot::updatePlotPath()
+{
+    jassert (generatePlotCallback != nullptr);
+    auto [xBuffer, yBuffer] = generatePlotCallback();
+
+    jassert (! xBuffer.empty() && ! yBuffer.empty());
+    jassert (xBuffer.size() == yBuffer.size());
+    plotPath.clear();
+    plotPath.preallocateSpace (3 * (int) xBuffer.size());
+
+    const auto getPoint = [this] (float xAmp, float yAmp) -> juce::Point<float>
+    {
+        return { getXCoordinateForAmplitude (xAmp), getYCoordinateForAmplitude (yAmp) };
+    };
+
+    bool pathStarted = false;
+    for (auto [xAmp, yAmp] : chowdsp::zip (xBuffer, yBuffer))
+    {
+        if (! pathStarted)
+        {
+            plotPath.startNewSubPath (getPoint (xAmp, yAmp));
+            pathStarted = true;
+        }
+        else
+        {
+            plotPath.lineTo (getPoint (xAmp, yAmp));
+        }
+    }
+}
+
+void WaveshaperPlot::setSize (juce::Rectangle<int> newBounds)
+{
+    bounds = newBounds;
+    updatePlotPath();
+}
+} // namespace chowdsp
