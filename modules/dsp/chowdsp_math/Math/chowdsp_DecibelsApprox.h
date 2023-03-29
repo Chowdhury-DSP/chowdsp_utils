dd69587033cbea912b45bcfab0251afb74c4375a
jatin <jatinchowdhury18@gmail.com>
2023-03-29 13:48:42 -0700

    Add optional orders for math approximations

--- modules/dsp/chowdsp_math/Math/chowdsp_DecibelsApprox.h
+++ modules/dsp/chowdsp_math/Math/chowdsp_DecibelsApprox.h
@@ -8,21 +8,21 @@ namespace chowdsp
 namespace DecibelsApprox
 {
     /** Approximate version of juce::decibelsToGain */
-    template <typename T>
+    template <typename T, int order = 3>
     inline T gainToDecibels (T gain, T minusInfinityDb = SampleTypeHelpers::NumericType<T> (-100))
     {
         CHOWDSP_USING_XSIMD_STD (max);
         return SIMDUtils::select (gain > T {},
-                                  max (LogApprox::log10 (gain) * (SampleTypeHelpers::NumericType<T>) 20, minusInfinityDb),
+                                  max (LogApprox::log10<T, order> (gain) * (SampleTypeHelpers::NumericType<T>) 20, minusInfinityDb),
                                   minusInfinityDb);
     }
 
     /** Approximate version of juce::gainToDecibels */
-    template <typename T>
+    template <typename T, int order = 3>
     inline T decibelsToGain (T decibels, T minusInfinityDb = SampleTypeHelpers::NumericType<T> (-100))
     {
         return SIMDUtils::select (decibels > minusInfinityDb,
-                                  PowApprox::pow10 (decibels * (SampleTypeHelpers::NumericType<T>) 0.05),
+                                  PowApprox::pow10<T, order> (decibels * (SampleTypeHelpers::NumericType<T>) 0.05),
                                   T {});
     }
 } // namespace DecibelsApprox
--- modules/dsp/chowdsp_math/Math/chowdsp_LogApprox.h
+++ modules/dsp/chowdsp_math/Math/chowdsp_LogApprox.h
@@ -16,28 +16,62 @@ namespace LogApprox
         using namespace SampleTypeHelpers;
 
         /** approximation for log_2(x), optimized on the range [1, 2] */
-        template <typename T>
+        template <typename T, int order>
         constexpr T log2_approx (T x)
         {
-            constexpr auto alpha = (T) 0.1640425613334452;
-            constexpr auto beta = (T) -1.098865286222744;
-            constexpr auto gamma = (T) 3.148297929334117;
-            constexpr auto zeta = (T) -2.213475204444817;
+            // The first-order approximation just matches the endpoints (i.e. {0, 1} and {1, 2})
+            // The second-order approximation matches the endpoints and the midpoint
+            // The third-order approximation matches the endpoints, and the slopes at the endpoints
 
-            return estrin<3> ({ alpha, beta, gamma, zeta }, x);
+            static_assert (order >= 1 && order <= 3);
+
+            if constexpr (order == 3)
+            {
+                constexpr auto alpha = (T) 0.1640425613334452;
+                constexpr auto beta = (T) -1.098865286222744;
+                constexpr auto gamma = (T) 3.148297929334117;
+                constexpr auto zeta = (T) -2.213475204444817;
+                return chowdsp::Polynomials::estrin<3> ({ alpha, beta, gamma, zeta }, x);
+            }
+            else if constexpr (order == 2)
+            {
+                constexpr auto beta = (T) -0.33985;
+                constexpr auto gamma = (T) 2.01955;
+                constexpr auto zeta = (T) -1.6797;
+                return chowdsp::Polynomials::estrin<2> ({ beta, gamma, zeta }, x);
+            }
+            else if constexpr (order == 1)
+            {
+                return x - (T) 1;
+            }
         }
 
 #if ! CHOWDSP_NO_XSIMD
         /** approximation for log_2(x), optimized on the range [1, 2] */
-        template <typename T>
+        template <typename T, int order>
         inline xsimd::batch<T> log2_approx (const xsimd::batch<T>& x)
         {
-            static constexpr auto alpha = (NumericType<T>) 0.1640425613334452;
-            static constexpr auto beta = (NumericType<T>) -1.098865286222744;
-            static constexpr auto gamma = (NumericType<T>) 3.148297929334117;
-            static constexpr auto zeta = (NumericType<T>) -2.213475204444817;
+            static_assert (order >= 1 && order <= 3);
 
-            return estrin<3> ({ alpha, beta, gamma, zeta }, x);
+            if constexpr (order == 3)
+            {
+                constexpr auto alpha = (T) 0.1640425613334452;
+                constexpr auto beta = (T) -1.098865286222744;
+                constexpr auto gamma = (T) 3.148297929334117;
+                constexpr auto zeta = (T) -2.213475204444817;
+                return chowdsp::Polynomials::estrin<3> ({ alpha, beta, gamma, zeta }, x);
+            }
+            else if constexpr (order == 2)
+            {
+                constexpr auto beta = (T) -0.33985;
+                constexpr auto gamma = (T) 2.01955;
+                constexpr auto zeta = (T) -1.6797;
+                return chowdsp::Polynomials::estrin<2> ({ beta, gamma, zeta }, x);
+            }
+            else if constexpr (order == 1)
+            {
+                return x - (T) 1;
+            }
         }
 #endif
 
@@ -45,8 +79,8 @@ namespace LogApprox
     } // namespace detail
 
     /** approximation for log(x) (32-bit) */
-    template <typename Base = detail::Euler>
-    inline float log (float x)
+    template <typename Base, int order = 3>
+    inline float logarithm (float x)
     {
         const auto vi = reinterpret_cast<int32_t&> (x); // NOSONAR
         const auto ex = vi & 0x7f800000;
@@ -55,12 +89,12 @@ namespace LogApprox
         const auto vf = reinterpret_cast<const float&> (vfi); // NOSONAR
 
         static constexpr auto log2_base_r = 1.0f / gcem::log2 (Base::template value<float>);
-        return log2_base_r * ((float) e + detail::log2_approx<float> (vf));
+        return log2_base_r * ((float) e + detail::log2_approx<float, order> (vf));
     }
 
     /** approximation for log(x) (64-bit) */
-    template <typename Base = detail::Euler>
-    inline double log (double x)
+    template <typename Base, int order = 3>
+    inline double logarithm (double x)
     {
         const auto vi = reinterpret_cast<int64_t&> (x); // NOSONAR
         const auto ex = vi & 0x7ff0000000000000;
@@ -69,13 +103,13 @@ namespace LogApprox
         const auto vf = reinterpret_cast<const double&> (vfi); // NOSONAR
 
         static constexpr auto log2_base_r = 1.0 / gcem::log2 (Base::template value<double>);
-        return log2_base_r * ((double) e + detail::log2_approx<double> (vf));
+        return log2_base_r * ((double) e + detail::log2_approx<double, order> (vf));
     }
 
 #if ! CHOWDSP_NO_XSIMD
     /** approximation for log(x) (SIMD 32-bit) */
-    template <typename Base = detail::Euler>
-    inline xsimd::batch<float> log (xsimd::batch<float> x)
+    template <typename Base, int order = 3>
+    inline xsimd::batch<float> logarithm (xsimd::batch<float> x)
     {
         const auto vi = reinterpret_cast<xsimd::batch<int32_t>&> (x); // NOSONAR
         const auto ex = vi & 0x7f800000;
@@ -84,12 +118,12 @@ namespace LogApprox
         const auto vf = reinterpret_cast<const xsimd::batch<float>&> (vfi); // NOSONAR
 
         static constexpr auto log2_base_r = 1.0f / gcem::log2 (Base::template value<float>);
-        return log2_base_r * (xsimd::to_float (e) + detail::log2_approx<float> (vf));
+        return log2_base_r * (xsimd::to_float (e) + detail::log2_approx<float, order> (vf));
     }
 
     /** approximation for log(x) (SIMD 64-bit) */
-    template <typename Base = detail::Euler>
-    inline xsimd::batch<double> log (xsimd::batch<double> x)
+    template <typename Base, int order = 3>
+    inline xsimd::batch<double> logarithm (xsimd::batch<double> x)
     {
         const auto vi = reinterpret_cast<xsimd::batch<int64_t>&> (x); // NOSONAR
         const auto ex = vi & 0x7ff0000000000000;
@@ -98,20 +132,26 @@ namespace LogApprox
         const auto vf = reinterpret_cast<const xsimd::batch<double>&> (vfi); // NOSONAR
 
         static constexpr auto log2_base_r = 1.0 / gcem::log2 (Base::template value<double>);
-        return log2_base_r * (xsimd::to_float (e) + detail::log2_approx<double> (vf));
+        return log2_base_r * (xsimd::to_float (e) + detail::log2_approx<double, order> (vf));
     }
 #endif
 
-    template <typename T>
-    inline T log10 (T x)
+    template <typename T, int order = 3>
+    inline T log (T x)
     {
-        return log<Ratio<10, 1>> (x);
+        return logarithm<detail::Euler, order> (x);
     }
 
-    template <typename T>
+    template <typename T, int order = 3>
+    inline T log10 (T x)
+    {
+        return logarithm<Ratio<10, 1>, order> (x);
+    }
+
+    template <typename T, int order = 3>
     inline T log2 (T x)
     {
-        return log<Ratio<2, 1>> (x);
+        return logarithm<Ratio<2, 1>, order> (x);
     }
 } // namespace LogApprox
 } // namespace chowdsp
