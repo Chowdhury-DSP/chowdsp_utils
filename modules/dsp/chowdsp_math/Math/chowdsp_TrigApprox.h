535f8b8af50e2c148f87b18ddf31f56ac4626049
jatin <jatinchowdhury18@gmail.com>
2023-10-10 13:07:17 -0700

    Add cosine approximations

--- modules/dsp/chowdsp_math/Math/chowdsp_TrigApprox.h
+++ modules/dsp/chowdsp_math/Math/chowdsp_TrigApprox.h
@@ -7,7 +7,6 @@ namespace chowdsp
  *
  * References:
  * - Sine plots: https://www.desmos.com/calculator/rnsmcx6wb5
- * - Taylor polynomials for sine triple-angle approx.: https://www.wolframcloud.com/env/chowdsp/sin_approx.nb
  */
 namespace TrigApprox
 {
@@ -39,6 +38,24 @@ namespace TrigApprox
             return SIMDUtils::select (x >= (T) 0, mod, mod + juce::MathConstants<NumericType>::twoPi) - juce::MathConstants<NumericType>::pi;
         }
 
+        /** Shifts range [-pi, pi] to [-pi/2, 3pi/2] for cosine calculations */
+        template <typename T>
+        T shift_cosine_range (T x)
+        {
+            using NumericType = SampleTypeHelpers::NumericType<T>;
+            return SIMDUtils::select (x >= -juce::MathConstants<NumericType>::halfPi,
+                                      x,
+                                      x + juce::MathConstants<NumericType>::twoPi);
+        }
+
+        /**
+         * Polynomial approximations of sine for [-pi/3, pi]
+         *
+         * Starts as a Taylor approximation, but then tries to fix the end-points,
+         * minimize max error.
+         *
+         * Reference: https://www.wolframcloud.com/env/chowdsp/sin_approx.nb
+         */
         template <typename T, int order>
         T sin_o3 (T theta, T theta_sq, [[maybe_unused]] T theta_quad)
         {
@@ -69,6 +86,41 @@ namespace TrigApprox
 
                 const auto s1 = NumericType (1) + NumericType (-1.57763153266e-1) * theta_sq;
                 return theta * s1;
+            }
+        }
+
+        /**
+         * Polynomial approximations of cosine for [-pi/3, pi]
+         *
+         * Starts as a Taylor approximation, but then tries to fix the end-points,
+         * minimize max error.
+         *
+         * Reference: https://www.wolframcloud.com/env/chowdsp/cos_approx.nb
+         *
+         * @TODO: I think I can get the error for these down a bit lower, just need some more math/Mathematica tricks
+         */
+        template <typename T, int order>
+        T cos_o3 ([[maybe_unused]] T theta, T theta_sq, [[maybe_unused]] T theta_quad)
+        {
+            using NumericType = SampleTypeHelpers::NumericType<T>;
+            static_assert (order % 2 == 0 && order > 2 && order <= 8, "Order must be an odd integer within [2,8]");
+
+            if constexpr (order == 8)
+            {
+                const auto c1 = (NumericType) -0.5 + (NumericType) 4.1666570372109e-2 * theta_sq;
+                const auto c2 = (NumericType) -1.3884536712849e-3 + (NumericType) 2.4185513623303e-5 * theta_sq;
+                return (NumericType) 1 + theta_sq * (c1 + c2 * theta_quad);
+            }
+            else if constexpr (order == 6)
+            {
+                const auto c1 = (NumericType) -0.5 + (NumericType) 4.165121515e-2 * theta_sq;
+                const auto c2 = (NumericType) -1.348103430e-3 * theta_quad;
+                return (NumericType) 1 + theta_sq * (c1 + c2);
+            }
+            else if constexpr (order == 4)
+            {
+                const auto c1 = (NumericType) -0.5 + (NumericType) 4.017380231e-2 * theta_sq;
+                return (NumericType) 1 + theta_sq * c1;
             }
         }
     } // namespace detail
@@ -170,10 +222,99 @@ namespace TrigApprox
         return sin_3angle_mpi_pi<order, T> (detail::fast_mod_mpi_pi (x));
     }
 
+    /**
+     * Bhaskara I's cosine approximation valid for x in [-pi, pi]
+     *
+     * Max error: 1.64e-3
+     *
+     * Reference: https://en.wikipedia.org/wiki/Bh%C4%81skara_I%27s_sine_approximation_formula
+     */
+    template <typename T>
+    T cos_bhaskara_mpi_pi (T x)
+    {
+        using NumericType = SampleTypeHelpers::NumericType<T>;
+        static constexpr auto pi = juce::MathConstants<NumericType>::pi;
+        jassert (SIMDUtils::all (x <= pi)
+                 && SIMDUtils::all (x >= -pi));
+
+        x = detail::shift_cosine_range (x);
+        const auto needs_flip = x > juce::MathConstants<NumericType>::halfPi;
+
+        x = SIMDUtils::select (needs_flip, x - pi, x);
+
+        static constexpr auto pi_sq = pi * pi;
+        const auto x_sq = x * x;
+        const auto result = (pi_sq - (NumericType) 4 * x_sq) / (pi_sq + x_sq);
+        return SIMDUtils::select (needs_flip, -result, result);
+    }
+
+    /** Bhaskara I's cosine approximation valid across the whole range. */
+    template <typename T>
+    T cos_bhaskara (T x)
+    {
+        return cos_bhaskara_mpi_pi (detail::fast_mod_mpi_pi (x));
+    }
+
+    /**
+     * First-order cosine approximation.
+     *
+     * Max error: 5.6e-2
+     */
+    template <typename T>
+    T cos_1st_order_mpi_pi (T x)
+    {
+        using NumericType = SampleTypeHelpers::NumericType<T>;
+        [[maybe_unused]] static constexpr auto pi = juce::MathConstants<NumericType>::pi;
+        jassert (SIMDUtils::all (x <= pi)
+                 && SIMDUtils::all (x >= -pi));
+
+        x = detail::shift_cosine_range (x);
+        return -sin_1st_order_mpi_pi (x - juce::MathConstants<NumericType>::halfPi);
+    }
+
+    /** Full-range first-order cosine approximation. */
+    template <typename T>
+    T cos_1st_order (T x)
+    {
+        return cos_1st_order_mpi_pi (detail::fast_mod_mpi_pi (x));
+    }
+
+    /**
+     * Sine approximation using a Taylor approximation on the
+     * range [-pi/3, pi/3], expanded to [-pi, pi] using the
+     * triple angle formula.
+     *
+     * The approximation is parameterized on the order of
+     * the Taylor approximation, which _must_ be an odd integer.
+     *
+     * Max error (8th-order): 8.7e-9
+     * Max error (6th-order): 2.82e-6
+     * Max error (4th-order): 7.93e-4
+     */
+    template <int order = 6, typename T = float>
+    T cos_3angle_mpi_pi (T x)
+    {
+        using NumericType = SampleTypeHelpers::NumericType<T>;
+        [[maybe_unused]] static constexpr auto pi = juce::MathConstants<NumericType>::pi;
+        jassert (SIMDUtils::all (x <= pi)
+                 && SIMDUtils::all (x >= -pi));
+
+        const auto theta_o3 = x * NumericType (1.0 / 3.0);
+        const auto theta_o3_sq = theta_o3 * theta_o3;
+        const auto theta_o3_quad = theta_o3_sq * theta_o3_sq;
+        const auto cos_o3 = detail::cos_o3<T, order> (theta_o3, theta_o3_sq, theta_o3_quad);
+        return cos_o3 * ((NumericType) -3 + (NumericType) 4 * cos_o3 * cos_o3);
+    }
+
+    /** Full-range triple-angle sine approximation. */
+    template <int order = 6, typename T = float>
+    T cos_3angle (T x)
+    {
+        return cos_3angle_mpi_pi<order, T> (detail::fast_mod_mpi_pi (x));
+    }
+
     // @TODO:
-    // - cosine
     // - sine/cosine together
-    // - more tests
     // - combined sine function with enum template?
 } // namespace TrigApprox
 } // namespace chowdsp
--- tests/dsp_tests/chowdsp_math_test/TrigApproxTest.cpp
+++ tests/dsp_tests/chowdsp_math_test/TrigApproxTest.cpp
@@ -12,6 +12,9 @@ TEMPLATE_TEST_CASE ("Trig Approx Test", "[dsp][math][simd]", float, double, xsim
     {
         const auto tester = [] (FloatType (*testFunc) (FloatType), NumericType margin)
         {
+            if constexpr (std::is_same_v<NumericType, float>)
+                margin = std::sqrt (margin);
+
             static constexpr auto pi10 = (NumericType) 10 * juce::MathConstants<NumericType>::pi;
             static constexpr auto inc = (NumericType) 0.001 * juce::MathConstants<NumericType>::pi;
             for (auto x = -pi10; x < pi10; x += inc)
@@ -24,9 +27,33 @@ TEMPLATE_TEST_CASE ("Trig Approx Test", "[dsp][math][simd]", float, double, xsim
 
         tester (&ta::sin_bhaskara, (NumericType) 1.65e-3);
         tester (&ta::sin_1st_order, (NumericType) 5.65e-2);
-        tester (&ta::sin_3angle<9>, (NumericType) 3.5e-10);
-        tester (&ta::sin_3angle<7>, (NumericType) 1.15e-7);
-        tester (&ta::sin_3angle<5>, (NumericType) 4.35e-5);
-        tester (&ta::sin_3angle<3>, (NumericType) 7.35e-3);
+        tester (&ta::sin_3angle<9, FloatType>, (NumericType) 3.5e-10);
+        tester (&ta::sin_3angle<7, FloatType>, (NumericType) 1.15e-7);
+        tester (&ta::sin_3angle<5, FloatType>, (NumericType) 4.35e-5);
+        tester (&ta::sin_3angle<3, FloatType>, (NumericType) 7.35e-3);
+    }
+
+    SECTION ("Cosine")
+    {
+        const auto tester = [] (FloatType (*testFunc) (FloatType), NumericType margin)
+        {
+            if constexpr (std::is_same_v<NumericType, float>)
+                margin = std::sqrt (margin);
+
+            static constexpr auto pi10 = (NumericType) 10 * juce::MathConstants<NumericType>::pi;
+            static constexpr auto inc = (NumericType) 0.001 * juce::MathConstants<NumericType>::pi;
+            for (auto x = -pi10; x < pi10; x += inc)
+            {
+                const auto ref = std::cos (x);
+                const auto input = (FloatType) x;
+                REQUIRE (testFunc (input) == SIMDApprox<FloatType> { ref }.margin (margin));
+            }
+        };
+
+        tester (&ta::cos_bhaskara, (NumericType) 1.65e-3);
+        tester (&ta::cos_1st_order, (NumericType) 5.65e-2);
+        tester (&ta::cos_3angle<8, FloatType>, (NumericType) 8.7e-9);
+        tester (&ta::cos_3angle<6, FloatType>, (NumericType) 2.82e-6);
+        tester (&ta::cos_3angle<4, FloatType>, (NumericType) 7.93e-4);
     }
 }
