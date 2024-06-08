14258e7eb5197875aca6fd7b93342d057c8271c4
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2024-06-08 01:12:15 -0700

    Data Structures: Enum Map (#536)
    * RawObject improvements
    
    * Initial work on Enum Map
    
    * Complete but not fully tested enum map
    
    * Add clear and emplace methods
    
    * More tests
    
    * Apply clang-format
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/common/chowdsp_data_structures/chowdsp_data_structures.h
+++ modules/common/chowdsp_data_structures/chowdsp_data_structures.h
@@ -31,11 +31,12 @@ BEGIN_JUCE_MODULE_DECLARATION
 
 #include "Structures/chowdsp_DoubleBuffer.h"
 #include "Structures/chowdsp_DestructiblePointer.h"
+#include "Structures/chowdsp_RawObject.h"
 #include "Structures/chowdsp_LocalPointer.h"
 #include "Structures/chowdsp_OptionalPointer.h"
-#include "Structures/chowdsp_RawObject.h"
 #include "Structures/chowdsp_SmallVector.h"
 #include "Structures/chowdsp_StringLiteral.h"
+#include "Structures/chowdsp_EnumMap.h"
 
 #include "Allocators/chowdsp_ArenaAllocator.h"
 #include "Allocators/chowdsp_ChainedArenaAllocator.h"
