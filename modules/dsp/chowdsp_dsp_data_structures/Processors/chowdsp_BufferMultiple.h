95b945b996a1b3377a9fd8db38fefb3b236b3eb5
jatin <jatinchowdhury18@gmail.com>
2024-10-29 12:24:02 -0700

    Buffer multiple passes tests

--- modules/dsp/chowdsp_dsp_data_structures/Processors/chowdsp_BufferMultiple.h
+++ modules/dsp/chowdsp_dsp_data_structures/Processors/chowdsp_BufferMultiple.h
@@ -8,72 +8,119 @@ class BufferMultiple
 public:
     BufferMultiple() = default;
 
-    void prepare (const juce::dsp::ProcessSpec& spec, int multiple)
+    int prepare (const juce::dsp::ProcessSpec& spec, int multiple)
     {
-        jassert (multiple > 1 && multiple < 8);
+        jassert (multiple > 1 && multiple <= static_cast<int> (maxMultiple));
         M = multiple;
-
-        maxBufferSize = static_cast<int> (spec.maximumBlockSize);
         numChannels = static_cast<int> (spec.numChannels);
 
         reset();
+
+        return Math::round_to_next_multiple (Math::round_to_next_multiple (static_cast<int> (spec.maximumBlockSize), M),
+                                             static_cast<int> (SIMDUtils::defaultSIMDAlignment / sizeof (T)));
     }
 
     void reset() noexcept
     {
-        data.fill (T {});
-        numSamplesLeftover = M - 1;
+        leftoverDataIn.fill (T {});
+        leftoverDataOut.fill (T {});
+        numSamplesLeftoverIn = M - 1;
+        numSamplesLeftoverOut = 1;
+    }
+
+    [[nodiscard]] int getMultipliedBufferLatency() const noexcept
+    {
+        return M - 1;
+    }
+
+    [[nodiscard]] int getRoundTripLatency() const noexcept
+    {
+        return M;
     }
 
     BufferView<T> processBufferIn (ArenaAllocatorView arena, const BufferView<const T>& input) noexcept
     {
         const auto numSamplesIn = input.getNumSamples();
-        jassert (numSamplesIn >= 2 * M);
-
-        const auto numMultiplesOut = std::min (numSamplesIn + numSamplesLeftover - (M - 1), maxBufferSize) / M;
+        const auto numMultiplesOut = (numSamplesIn + numSamplesLeftoverIn - 1) / M;
         const auto numSamplesOut = M * numMultiplesOut;
-        const auto newNumLeftoverSamples = numSamplesIn + numSamplesLeftover - numSamplesOut;
+        const auto newNumLeftoverSamples = numSamplesIn + numSamplesLeftoverIn - numSamplesOut;
         jassert (newNumLeftoverSamples <= M);
 
-        auto leftoverSamplesToUse = std::min (numSamplesLeftover, numSamplesOut);
+        auto leftoverSamplesToUse = std::min (numSamplesLeftoverIn, numSamplesOut);
 
         const auto bufferOut = make_temp_buffer<T> (arena, input.getNumChannels(), numSamplesOut);
 
         for (auto [ch, outData] : buffer_iters::channels (bufferOut))
         {
             const auto inputs = input.getReadSpan (ch);
-            const auto leftovers = getLeftovers (ch);
+            const auto leftovers = getLeftoversIn (ch);
 
             std::copy (leftovers.begin(), leftovers.begin() + leftoverSamplesToUse, outData.begin());
             std::copy (inputs.begin(), inputs.begin() + numSamplesOut - leftoverSamplesToUse, outData.begin() + leftoverSamplesToUse);
 
-            if (leftoverSamplesToUse < numSamplesLeftover)
+            if (leftoverSamplesToUse < numSamplesLeftoverIn)
             {
                 std::copy (leftovers.begin() + leftoverSamplesToUse, leftovers.end(), leftovers.begin());
             }
 
             std::copy (inputs.begin() + numSamplesOut - leftoverSamplesToUse,
                        inputs.end(),
-                       leftovers.begin() + numSamplesLeftover - leftoverSamplesToUse);
-            numSamplesLeftover = newNumLeftoverSamples;
+                       leftovers.begin() + numSamplesLeftoverIn - leftoverSamplesToUse);
         }
+        numSamplesLeftoverIn = newNumLeftoverSamples;
 
         return bufferOut;
     }
 
-    void processBufferOut (const BufferView<const T>& input, const BufferView<T>& output) noexcept;
+    void processBufferOut (const BufferView<const T>& input, const BufferView<T>& output) noexcept
+    {
+        const auto numSamplesIn = input.getNumSamples();
+        const auto numSamplesOut = output.getNumSamples();
+        const auto newNumLeftoverSamples = numSamplesIn + numSamplesLeftoverOut - numSamplesOut;
+        jassert (newNumLeftoverSamples <= M);
+
+        auto leftoverSamplesToUse = std::min (numSamplesLeftoverOut, numSamplesOut);
+
+        for (auto [ch, outData] : buffer_iters::channels (output))
+        {
+            const auto inputs = input.getReadSpan (ch);
+            const auto leftovers = getLeftoversOut (ch);
+
+            std::copy (leftovers.begin(), leftovers.begin() + leftoverSamplesToUse, outData.begin());
+            std::copy (inputs.begin(), inputs.begin() + numSamplesOut - leftoverSamplesToUse, outData.begin() + leftoverSamplesToUse);
+
+            if (leftoverSamplesToUse < numSamplesLeftoverOut)
+            {
+                std::copy (leftovers.begin() + leftoverSamplesToUse, leftovers.end(), leftovers.begin());
+            }
+
+            std::copy (inputs.begin() + numSamplesOut - leftoverSamplesToUse,
+                       inputs.end(),
+                       leftovers.begin() + numSamplesLeftoverOut - leftoverSamplesToUse);
+        }
+
+        numSamplesLeftoverOut = newNumLeftoverSamples;
+    }
 
 private:
-    nonstd::span<T> getLeftovers (int ch)
+    nonstd::span<T> getLeftoversIn (int ch)
     {
-        return { data.data() + ch * M, static_cast<size_t> (M) };
+        return { leftoverDataIn.data() + ch * M, static_cast<size_t> (M) };
+    }
+
+    nonstd::span<T> getLeftoversOut (int ch)
+    {
+        return { leftoverDataOut.data() + ch * M, static_cast<size_t> (M) };
     }
 
     int M = 0;
-    int maxBufferSize = 0;
     int numChannels = 0;
-    int numSamplesLeftover = 0;
-    std::array<T, CHOWDSP_BUFFER_MAX_NUM_CHANNELS * 16> data {};
+    int numSamplesLeftoverIn = 0;
+    int numSamplesLeftoverOut = -1;
+
+    static constexpr size_t maxMultiple = 8;
+    std::array<T, CHOWDSP_BUFFER_MAX_NUM_CHANNELS * maxMultiple> leftoverDataIn {};
+    std::array<T, CHOWDSP_BUFFER_MAX_NUM_CHANNELS * maxMultiple> leftoverDataOut {};
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BufferMultiple)
 };
--- tests/dsp_tests/chowdsp_dsp_data_structures_test/BufferMultipleTest.cpp
+++ tests/dsp_tests/chowdsp_dsp_data_structures_test/BufferMultipleTest.cpp
@@ -1,36 +1,56 @@
 #include <CatchUtils.h>
 #include <chowdsp_dsp_data_structures/chowdsp_dsp_data_structures.h>
 
+#include <iostream>
+
 TEST_CASE ("Buffer Multiple Test", "[dsp][data-structures]")
 {
-    static constexpr int numChannels = 1;
+    static constexpr int numChannels = 2;
     static constexpr int bufferSize = 64;
     static constexpr int N = 1000;
-    std::vector<float> data (N, 0.0f);
-    std::iota (std::begin (data), std::end (data), 0.0f);
 
-    chowdsp::ArenaAllocator<std::array<std::byte, bufferSize * sizeof (float)>> arena {};
-
-    for (int multiple : std::initializer_list<int> { 2 }) //, 3, 4, 5, 6, 7 })
+    for (int multiple : std::initializer_list<int> { 2, 3, 4, 5, 6, 7 })
     {
-        chowdsp::BufferMultiple<float> multipleProcessor {};
-        multipleProcessor.prepare ({ 0.0, (uint32_t) bufferSize, (uint32_t) numChannels }, multiple);
+        std::vector<float> data (N, 0.0f);
+        std::iota (std::begin (data), std::end (data), 0.0f);
+        std::vector<float> data2 { data.begin(), data.end() };
+        float* dataPtrs[numChannels] = { data.data(), data2.data() };
 
-        int inCount = 0;
+        chowdsp::BufferMultiple<float> multipleProcessor {};
+        int maxMultipleBufferSize = multipleProcessor.prepare ({ 0.0, (uint32_t) bufferSize, (uint32_t) numChannels }, multiple);
+        const auto mLatencySamples = multipleProcessor.getMultipliedBufferLatency();
+        const auto roundTripLatencySamples = multipleProcessor.getRoundTripLatency();
+
+        chowdsp::ArenaAllocator<> arena { numChannels * maxMultipleBufferSize * sizeof (float) };
+
+        int inOutCount = 0;
         int mCount = 0;
-        int outCount = 0;
-        for (int n : std::initializer_list<int> { 6, 20, 1, 3, 15, 16, 5, 7, 18 })
+        for (int n : std::initializer_list<int> { 6, 20, 1, 3, 15, 16, 5, 7, 18, bufferSize, bufferSize, bufferSize })
         {
-            chowdsp::BufferView buffer { data.data(), n, inCount };
+            chowdsp::BufferView buffer { dataPtrs, numChannels, n, inOutCount };
             const auto mBuffer = multipleProcessor.processBufferIn (arena, buffer);
 
             REQUIRE (mBuffer.getNumSamples() % multiple == 0);
-            for (auto [idx, x] : chowdsp::enumerate (mBuffer.getWriteSpan (0)))
+            REQUIRE (mBuffer.getNumSamples() <= maxMultipleBufferSize);
+            for (auto [ch, channelData] : chowdsp::buffer_iters::channels (mBuffer))
             {
-                REQUIRE (x == std::max (static_cast<float> (mCount + (int) idx - (multiple - 1)), 0.0f));
+                for (const auto& [idx, x] : chowdsp::enumerate (channelData))
+                {
+                    REQUIRE (x == std::max (static_cast<float> (mCount + (int) idx - mLatencySamples), 0.0f));
+                    x = -x;
+                }
             }
 
-            inCount += n;
+            multipleProcessor.processBufferOut (mBuffer, buffer);
+            for (auto [ch, channelData] : chowdsp::buffer_iters::channels (buffer))
+            {
+                for (const auto& [idx, x] : chowdsp::enumerate (channelData))
+                {
+                    REQUIRE (-x == std::max (static_cast<float> (inOutCount + (int) idx - roundTripLatencySamples), 0.0f));
+                }
+            }
+
+            inOutCount += n;
             mCount += mBuffer.getNumSamples();
             arena.clear();
         }
