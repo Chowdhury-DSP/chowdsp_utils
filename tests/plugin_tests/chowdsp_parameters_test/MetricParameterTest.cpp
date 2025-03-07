f34b26e323f473f79e164c5eb8ba56537d326213
jatin <jatinchowdhury18@gmail.com>
2025-03-07 00:02:27 -0800

    MetricParameter: allow text with 0 decimal places

--- tests/plugin_tests/chowdsp_parameters_test/MetricParameterTest.cpp
+++ tests/plugin_tests/chowdsp_parameters_test/MetricParameterTest.cpp
@@ -113,4 +113,38 @@ TEST_CASE ("Metric Parameter Test", "[plugin][parameters]")
         REQUIRE (param.getCurrentValueAsText() == juce::String::fromUTF8 ("-1.00 mV"));
         REQUIRE (getValueForText() == Catch::Approx { param.get() }.margin (1.0e-6));
     }
+
+    SECTION ("Decimal Places")
+    {
+        static constexpr auto test_val = 1.54321f;
+        static constexpr auto test_val_milli = 1.54321e-3f;
+        std::array<std::tuple<int, std::string_view, std::string_view>, 5> tests {
+            std::tuple<int, std::string_view, std::string_view> { 4, "1.5432 V", "1.5432 mV" },
+            { 3, "1.543 V", "1.543 mV" },
+            { 2, "1.54 V", "1.54 mV" },
+            { 1, "1.5 V", "1.5 mV" },
+            { 0, "1 V", "1 mV" },
+        };
+
+        for (auto [num_decimals, test_str, test_str_milli] : tests)
+        {
+            chowdsp::MetricParameter param {
+                "param",
+                "Param",
+                juce::NormalisableRange { -2.0f, 2.0f },
+                0.0f,
+                juce::String::fromUTF8 ("V"),
+                num_decimals,
+            };
+
+            param.setParameterValue (test_val);
+            REQUIRE (param.getCurrentValueAsText() == chowdsp::toString (test_str));
+
+            if (num_decimals < 4)
+            {
+                param.setParameterValue (test_val_milli);
+                REQUIRE (param.getCurrentValueAsText() == chowdsp::toString (test_str_milli));
+            }
+        }
+    }
 }
