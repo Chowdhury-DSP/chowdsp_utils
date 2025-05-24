c809df3c106feb82342226e61700b71c50ac6bc1
jatin <jatinchowdhury18@gmail.com>
2025-05-23 21:31:37 -0700

    Attachments: more accessor methods

--- modules/plugin/chowdsp_plugin_state/Frontend/chowdsp_SliderAttachment.h
+++ modules/plugin/chowdsp_plugin_state/Frontend/chowdsp_SliderAttachment.h
@@ -33,6 +33,7 @@ public:
     void setValue (float newValue);
 
     /** Returns the attached parameter */
+    [[nodiscard]] FloatParameter* getParameter() { return attachment.param; }
     [[nodiscard]] const FloatParameter* getParameter() const { return attachment.param; }
     [[nodiscard]] auto& getAttachment() { return attachment; }
 
