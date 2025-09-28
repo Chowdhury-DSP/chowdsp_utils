58ec4b9fc2ce97d84f83343620d772360925b82c
jatin <jatinchowdhury18@gmail.com>
2025-09-28 13:26:43 -0400

    Add ComponentArena::allocate_n_lambda

--- tests/gui_tests/chowdsp_gui_test/ComponentArenaTest.cpp
+++ tests/gui_tests/chowdsp_gui_test/ComponentArenaTest.cpp
@@ -6,7 +6,9 @@ TEST_CASE ("Component Arena Test", "[gui][data-structures]")
     struct TestComponent : juce::Component
     {
         int& destructor_counter;
+        size_t other_arg {};
         explicit TestComponent (int& dest_counter) : destructor_counter (dest_counter) {}
+        TestComponent (int& dest_counter, size_t other) : destructor_counter (dest_counter), other_arg (other) {}
         ~TestComponent() override { ++destructor_counter; }
     };
 
@@ -38,6 +40,24 @@ TEST_CASE ("Component Arena Test", "[gui][data-structures]")
         arena.allocate_n<TestComponent> (4, destructor_counter);
         arena.allocate_n<NotAComponent> (3, destructor_counter);
         REQUIRE (arena.component_count() == 5);
+
+        REQUIRE (destructor_counter == 0);
+        arena.clear_all();
+        REQUIRE (destructor_counter == 5);
+    }
+
+    SECTION ("allocate_n with args")
+    {
+        arena.allocate<TestComponent> (destructor_counter);
+        const auto array = arena.allocate_n_lambda<TestComponent> (4,
+                                                                   [&destructor_counter] (size_t idx)
+                                                                   {
+                                                                       return TestComponent { destructor_counter, idx * 2 + 1 };
+                                                                   });
+        REQUIRE (arena.component_count() == 5);
+
+        for (auto [idx, component] : chowdsp::enumerate (array))
+            REQUIRE (component.other_arg == 2 * idx + 1);
 
         REQUIRE (destructor_counter == 0);
         arena.clear_all();
